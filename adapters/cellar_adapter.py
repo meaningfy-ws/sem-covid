19de89bc38fa4236281160f77a6cdbf56268e7a1
Laurentiu Mandru <mclaurentiu79@gmail.com>
2021-02-02 14:19:25 +0200

    Modified treaties query and code. Added treaties ES index mapping

--- adapters/cellar_adapter.py
+++ adapters/cellar_adapter.py
@@ -6,12 +6,8 @@
 # Email: coslet.mihai@gmail.com
 import hashlib
 import logging
-import os
 import pathlib
-from pathlib import Path
-from typing import List
 from urllib.parse import quote_plus
-from uuid import uuid4
 
 import requests
 
@@ -32,130 +28,6 @@ class CellarAdapter:
         self.run = '+Run+Query+'
         self.max_query_size = 8000
 
-    def get_treaties(self, limit: int = None) -> dict:
-        """
-        Method to retrieve works of treaties and their metadata
-        :param limit: limit of query results
-        :return: dict with metadata
-        """
-        logger.debug(f'start retrieving works of treaties.')
-        query = self._limit_query("""prefix cdm: <http://publications.europa.eu/ontology/cdm#>
-        prefix lang: <http://publications.europa.eu/resource/authority/language/>
-        
-        select distinct ?work ?doc_id ?title ?comment ?eurovocConcept ?subjectMatter ?directoryCode ?dateCreated ?dateDocument ?legalDateSignature ?legalDateEntryIntoForce ?legalIdCelex ?legalEli  group_concat(?createdBy; separator=", ") as ?authors 
-        {
-          ?work a cdm:treaty;
-                  cdm:work_id_document ?doc_id.
-          optional {
-            ?work cdm:work_has_resource-type <http://publications.europa.eu/resource/authority/resource-type/TREATY>
-          }
-          optional {
-            ?work cdm:resource_legal_in-force "true"^^<http://www.w3.org/2001/XMLSchema#boolean>.
-          }
-          optional {
-              ?expression cdm:expression_belongs_to_work ?work. 
-              ?expression cdm:expression_title ?title.
-              # ?expression cdm:expression_uses_language lang:ENG.
-          }
-          optional {
-            ?work cdm:resource_legal_comment_internal ?comment .
-          }
-          optional {
-            ?work cdm:work_is_about_concept_eurovoc ?eurovocConcept 
-          }
-          optional {
-            ?work cdm:resource_legal_is_about_subject-matter ?subjectMatter 
-          }
-          optional {
-            ?work cdm:resource_legal_is_about_concept_directory-code ?directoryCode 
-          }
-          optional {
-            ?work cdm:work_created_by_agent ?createdBy .
-          }
-          optional {
-            ?work cdm:work_date_creation ?dateCreated .
-          }
-          optional {
-            ?work cdm:work_date_document ?dateDocument .
-          }
-          optional {
-            ?work cdm:resource_legal_date_signature ?legalDateSignature .
-          }
-          optional {
-            ?work cdm:resource_legal_date_entry-into-force ?legalDateEntryIntoForce .
-          }
-          optional {
-            ?work cdm:resource_legal_id_celex ?legalIdCelex .
-          }
-          optional {
-            ?work cdm:resource_legal_eli ?legalEli .
-          }
-          filter not exists{?work a cdm:fragment_resource_legal}.
-          filter not exists {?work cdm:work_embargo [].}
-        }""", limit)
-
-        response = self._make_request(query)
-        return response.json()
-
-    def get_treaty_items(self, treaties: List[str], format='html') -> dict:
-        """
-        Method to retrieve item for provided treaty works
-        :type format: accepted mime types
-        :type treaties: list of treaties to run the query against
-        :return: dict with metadata
-        """
-        logger.debug(f'start retrieving items of treaties.')
-
-        query = """prefix cdm: <http://publications.europa.eu/ontology/cdm#>
-        prefix cmr: <http://publications.europa.eu/ontology/cdm/cmr#>
-        prefix lang: <http://publications.europa.eu/resource/authority/language/>
-        select distinct ?item
-        {{
-          values ?work {{ {values} }}
-          values ?mime {{ ~format~ }}
-        
-          ?expression cdm:expression_belongs_to_work ?work. 
-          
-          ?manifestation cdm:manifestation_manifests_expression ?expression.
-        
-          ?item cdm:item_belongs_to_manifestation ?manifestation.
-          
-          ?item cmr:manifestationMimeType ?mime.
-        }}
-        """.replace('~format~', FORMATS[format])
-
-        result = {'results': {'bindings': list()}}
-        for full_query in self._chunk_query(query, treaties):
-            logger.debug('retrieving chunk...')
-            response = self._make_request(full_query)
-            result['results']['bindings'] += response.json()['results']['bindings']
-
-        logger.debug(f'start retrieving items of treaties.')
-        return result
-
-    def retrieve_document(self, path_to_save: str, url: str, format: str = 'html'):
-        """
-        Retrieve document over http to the specified path
-        :param path_to_save: location to save file
-        :param url: location to retrieve the file from
-        :param format: format of the document
-        :return: name of the file where the document is retrieved
-        """
-        logger.debug(f'start retrieving {url} in {format} format.')
-
-        response = requests.get(url)
-        if response.status_code != 200:
-            logger.debug(f'request on {url} returned {response.status_code} with {response.content}')
-            raise ValueError(f'request returned {response.status_code} with {response.content}')
-
-        file_name = str(uuid4()) + f'.{format}'
-        file_location = Path(path_to_save) / file_name
-        with open(file_location, 'wb') as file:
-            file.write(response.content)
-
-        logger.debug(f'finish retrieving {url} in {format} format.')
-        return file_name
-
     def _make_request(self, query):
         request_url = f'{self.url}/?default-graph-uri={self.default_graph}&format={self.format}&timeout={self.timeout}&debug={self.debug}&run={self.run}&query={quote_plus(query)}'
 
@@ -165,22 +37,6 @@ class CellarAdapter:
             raise ValueError(f'request on Virtuoso returned {response.status_code} with {response.content} body')
 
         return response
-
-    def _chunk_query(self, query: str, values_list: list):
-        values_str = ''
-        offset = 0
-        values_max = self.max_query_size - len(query)
-
-        while values_list[offset:]:
-            for value in values_list[offset:]:
-                value_to_add = f' <{value}>'
-                if len(values_str + value_to_add) >= values_max:
-                    break
-                values_str += value_to_add
-                offset += 1
-            print(values_str)
-            yield query.format(values=values_str)
-            values_str = ''
 
     def get_covid19_items(self):
         logger.debug(f'start retrieving works of treaties.')
@@ -271,8 +127,7 @@ class CellarAdapter:
         response = self._make_request(query)
         return response.json()
 
-    def download_covid19_items(self):
-        covid19_items = self.get_covid19_items()
+    def download_covid19_items(self, covid19_items):
         download_location = pathlib.Path('resources/covid19_eurlex')
 
         count = len(covid19_items['results']['bindings'])
@@ -281,6 +136,137 @@ class CellarAdapter:
         for item in covid19_items['results']['bindings']:
             current_item += 1
             filename = hashlib.sha256(item['title_']['value'].encode('utf-8')).hexdigest()
+
+            filename_pdf = filename + '_pdf.zip'
+            filename_html = filename + '_html.zip'
+
+            try:
+                print("Processing item " + str(current_item) + " of " + str(count))
+                url = item['pdf_to_download']['value'] if item['pdf_to_download']['value'].startswith('http') else (
+                        'http://' + item['pdf_to_download']['value'])
+                request = requests.get(url, allow_redirects=True)
+
+                with open(pathlib.Path(download_location) / str(filename_pdf), 'wb') as output_file:
+                    output_file.write(request.content)
+
+                url = item['html_to_download']['value'] if item['html_to_download']['value'].startswith('http') else (
+                        'http://' + item['html_to_download']['value'])
+                request = requests.get(url, allow_redirects=True)
+
+                with open(pathlib.Path(download_location) / str(filename_html), 'wb') as output_file:
+                    output_file.write(request.content)
+            except Exception as ex:
+                logger.exception(ex)
+
+    def get_treaty_items(self):
+        logger.debug(f'start retrieving works of treaties.')
+        query = """prefix cdm: <http://publications.europa.eu/ontology/cdm#>
+                    prefix lang: <http://publications.europa.eu/resource/authority/language/>
+                    
+                    select
+                    distinct ?work
+                    ?doc_id
+                    ?title
+                    ?comment
+                    group_concat(distinct ?eurovocConcept; separator=", ") as ?eurovocConcepts
+                    group_concat(distinct ?subjectMatter; separator=", ") as ?subjectMatters
+                    group_concat(distinct ?directoryCode; separator=", ") as ?directoryCodes
+                    ?dateCreated
+                    ?dateDocument
+                    ?legalDateSignature
+                    ?legalDateEntryIntoForce
+                    ?legalIdCelex
+                    ?oj_sector
+                    group_concat(distinct ?legalEli; separator=", ") as ?legalElis
+                    group_concat(?createdBy; separator=", ") as ?authors
+                    ?pdf_to_download
+                    ?html_to_download
+                    {
+                        ?work a cdm:treaty;
+                                cdm:work_id_document ?doc_id.
+                        OPTIONAL
+                        {
+                            ?work cdm:resource_legal_id_sector ?oj_sector
+                        }
+                        optional {
+                        ?work cdm:work_has_resource-type <http://publications.europa.eu/resource/authority/resource-type/TREATY>
+                        }
+                        optional {
+                        ?work cdm:resource_legal_in-force "true"^^<http://www.w3.org/2001/XMLSchema#boolean>.
+                        }
+                        optional {
+                            ?expression cdm:expression_belongs_to_work ?work. 
+                            ?expression cdm:expression_title ?title.
+                            ?expression cdm:expression_uses_language lang:ENG.
+                            
+                            OPTIONAL
+                            {
+                                ?manif_pdf cdm:manifestation_manifests_expression ?expression.
+                                ?manif_pdf cdm:manifestation_type ?type_pdf.
+                                FILTER(str(?type_pdf) in ('pdf', 'pdfa1a', 'pdfa2a', 'pdfa1b', 'pdfx'))
+                            }
+                            
+                            OPTIONAL
+                            {
+                                ?manif_html cdm:manifestation_manifests_expression ?expression.
+                                ?manif_html cdm:manifestation_type ?type_html.
+                                FILTER(str(?type_html) in ('html', 'xhtml'))
+                            }
+                    
+                            BIND(IRI(concat(?manif_pdf,"/zip")) as ?pdf_to_download)
+                            BIND(IRI(concat(?manif_html,"/zip")) as ?html_to_download)
+                        }
+                        optional {
+                        ?work cdm:resource_legal_comment_internal ?comment .
+                        }
+                        optional {
+                        ?work cdm:work_is_about_concept_eurovoc ?eurovocConcept 
+                        }
+                        optional {
+                        ?work cdm:resource_legal_is_about_subject-matter ?subjectMatter 
+                        }
+                        optional {
+                        ?work cdm:resource_legal_is_about_concept_directory-code ?directoryCode 
+                        }
+                        optional {
+                        ?work cdm:work_created_by_agent ?createdBy .
+                        }
+                        optional {
+                        ?work cdm:work_date_creation ?dateCreated .
+                        }
+                        optional {
+                        ?work cdm:work_date_document ?dateDocument .
+                        }
+                        optional {
+                        ?work cdm:resource_legal_date_signature ?legalDateSignature .
+                        }
+                        optional {
+                        ?work cdm:resource_legal_date_entry-into-force ?legalDateEntryIntoForce .
+                        }
+                        optional {
+                        ?work cdm:resource_legal_id_celex ?legalIdCelex .
+                        }
+                        optional {
+                        ?work cdm:resource_legal_eli ?legalEli .
+                        }
+                        filter not exists{?work a cdm:fragment_resource_legal}.
+                        filter not exists {?work cdm:work_embargo [].}
+                        FILTER EXISTS {?manif cdm:manifestation_manifests_expression ?expression}
+                    }
+                    ORDER BY ?dateDocument"""
+
+        response = self._make_request(query)
+        return response.json()
+
+    def download_treaty_items(self, treaty_items):
+        download_location = pathlib.Path('resources/treaties')
+
+        count = len(treaty_items['results']['bindings'])
+        current_item = 0
+
+        for item in treaty_items['results']['bindings']:
+            current_item += 1
+            filename = hashlib.sha256(item['title']['value'].encode('utf-8')).hexdigest()
 
             filename_pdf = filename + '_pdf.zip'
             filename_html = filename + '_html.zip'
