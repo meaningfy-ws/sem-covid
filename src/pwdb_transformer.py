88a90e6a7cae559849408e02c32c09ac9e7c883b
DanCh11 <dan.chiriac1453@gmail.com>
2021-02-11 11:52:07 +0200

    remade cleaning class

--- src/pwdb_transformer.py
+++ src/pwdb_transformer.py
@@ -7,22 +7,21 @@ from nltk.corpus import stopwords
 def cleaning(text):
     """cleaner function"""
     stopword = stopwords.words('english')
-
     # set text to lowercase
-    text.lower()
+    text = text.lower()
     # remove links
-    re.sub(r"^https?:\/\/.*[\r\n]*", '', text)
+    text = re.sub(r"^https?:\/\/.*[\r\n]*", '', text)
     # remove "new line" symbol
-    re.sub('\n', '', text)
+    text = re.sub('\n', '', text)
     # Match every decimal digits and every character marked as letters in Unicode database
-    re.sub('\w*\d\w*', '', text)
+    text = re.sub('\w*\d\w*', '', text)
     # Delete square brackets
-    re.sub('\[.*?\]', '', text)
-    re.sub('[‘’“”…]', '', text)
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('[‘’“”…]', '', text)
     # remove punctuation
-    re.sub('[%s]' % re.escape(string.punctuation), '', text)
-    ''.join(text)
-    re.split('\W+', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = ''.join(text)
+    text = re.split('\W+', text)
     text = [word for word in text if word not in stopword]
 
     return text
