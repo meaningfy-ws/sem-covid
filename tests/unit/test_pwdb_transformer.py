2c3246886974d8a8f2e3e324c63c4c8c6ce02f09
daycu <dan.chiriac1453@gmail.com>
2021-02-15 17:13:51 +0200

    Tested new transformer syntax

--- tests/unit/test_pwdb_transformer.py
+++ tests/unit/test_pwdb_transformer.py
@@ -1,13 +1,13 @@
 import json
 
-from src.pwdb_transformer import transform_json_to_csv
+from src.pwdb_transformer import transform_json_to_csv, get_transformation_rules
 
 
 def test_transform_json_to_csv_number_of_columns_and_rows(tmpdir):
     JSON = [{'fieldData': {'title': 'Hardship case fund: Safety net for self-employed',
                            'title_nationalLanguage': 'hdadsa'}}]
 
-    transformation_rules = '''{
+    transformation = '''{
             "Title": .fieldData.title,
             "Title (national language)": .fieldData.title_nationalLanguage
             }'''
@@ -15,16 +15,17 @@ def test_transform_json_to_csv_number_of_columns_and_rows(tmpdir):
     file = tmpdir.join("test_JSON.json")
     file.write(json.dumps(JSON))
 
-    transformer = transform_json_to_csv(file, transformation_rules)
+    transformation_rules = get_transformation_rules(transformation)
+    # Compile tranformation
+    df_transformed = transform_json_to_csv(file, transformation_rules)
 
     expected_list = ['Title', 'Title (national language)']
-    expected_number_of_rows = 2
 
-    df_rows = transformer.count()
-    df_columns = transformer.columns
+    df_rows = df_transformed.count()
+    df_columns = df_transformed.columns
 
     # compare number of rows
-    assert len(df_rows) == expected_number_of_rows
+    assert len(df_rows) == 2
 
     # compare number of columns
     assert len(df_columns) == len(expected_list)
