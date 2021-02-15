ec1f9d0781f77e6e5e1093bb8ee2e96f45753c5f
daycu <dan.chiriac1453@gmail.com>
2021-02-15 17:11:58 +0200

    Changed the name and improve the syntax

--- src/pwdb_transformer.py
+++ src/pwdb_transformer.py
@@ -3,12 +3,18 @@ import json
 import jq
 import pandas as pd
 
+SEARCH_RULE = ".[] | "
 
-def transform_json_to_csv(file: list, rules: str):
+
+def get_transformation_rules(rules: str, search_rule: str = SEARCH_RULE):
+    return (search_rule + rules).replace("\n", "")
+
+
+def transform_json_to_csv(file: str, transformed_rules: str):
     """
         assuming we have json file and transformation rules
         and we want to transform in into DataFrame format
-        :file_content: JSON file
+        :file: JSON file address
         :transformation_rules: dictionary based on
         jq library rules of transformation
     """
@@ -17,8 +23,7 @@ def transform_json_to_csv(file: list, rules: str):
         content = file.read()
         data = json.loads(content)
 
-    jq_transformation_program = (".[] | "+str(rules)).replace("\n", "")
-    transformation = jq.compile(jq_transformation_program)
+    transformation = jq.compile(transformed_rules)
 
     new_data = transformation.input(data).all()
     df_from_json = pd.DataFrame.from_records(new_data)
