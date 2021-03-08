5f31eb0457ad6b720ca508e0f42527912143918a
Mihai Coslet <coslet.mihai@gmail.com>
2021-03-08 16:36:48 +0200

    update eurlex and pwdb mappings and DAGs

--- dags/eurlex_covid19.py
+++ dags/eurlex_covid19.py
@@ -19,7 +19,7 @@ from tika import parser
 from dagtools.miniotools import MinioAdapter
 
 logger = logging.getLogger('lam-fetcher')
-VERSION = '0.9.8'
+VERSION = '0.10.0'
 
 URL: str = Variable.get('EURLEX_SPARQL_URL')
 APACHE_TIKA_URL = Variable.get('APACHE_TIKA_URL')
@@ -44,22 +44,36 @@ MINIO_ACCESS_KEY = Variable.get("MINIO_ACCESS_KEY")
 MINIO_SECRET_KEY = Variable.get("MINIO_SECRET_KEY")
 
 transformation = '''{
-title: .processed_title.value,
-date: .date_document.value,
-celex_id: .celex.value,
-oj: .full_oj.value,
-manifestation_html: .manifestation_html.value,
-manifestation_pdf: .manifestation_pdf.value,
-html_to_download: .html_to_download.value,
-pdf_to_download: .pdf_to_download.value,
-oj_sector: .oj_sector.value,
-resource_type: .resource_type.value,
-authors: .authors.value,
-eurovoc_concepts: .eurovoc_concepts.value,
-subject_matters: .subject_matters.value,
-directory_codes: .directory_codes.value,
-legal_elis: .legal_elis.value,
-content: .content
+work: .work.value,
+title: .title.value,
+cdm_types: .cdm_types.value | split("| "),
+cdm_type_labels: .cdm_type_labels.value | split("| "),
+resource_types: .resource_types.value | split("| "),
+resource_type_labels: .resource_type_labels.value | split("| "),
+eurovoc_concepts: .eurovoc_concepts.value | split("| "),
+eurovoc_concept_labels: .eurovoc_concept_labels.value | split("| "),
+subject_matters: .subject_matters.value | split("| "),
+subject_matter_labels: .subject_matter_labels.value | split("| "),
+directory_codes: .directory_codes.value | split("| "),
+directory_codes_labels: .directory_codes_labels.value | split("| "),
+celex_numbers: .celex_numbers.value | split("| "),
+legal_elis: .legal_elis.value | split("| "),
+id_documents: .id_documents.value | split("| "),
+same_as_uris: .same_as_uris.value | split("| "),
+authors: .authors.value | split("| "),
+author_labels: .author_labels.value | split("| "),
+full_ojs: .full_ojs.value | split("| "),
+oj_sectors: .oj_sectors.value | split("| "),
+internal_comments: .internal_comments.value | split("| "),
+is_in_force: .is_in_force.value | split("| "),
+dates_document: .dates_document.value | split("| "),
+dates_created: .dates_created.value | split("| "),
+legal_dates_entry_into_force: .legal_dates_entry_into_force.value | split("| "),
+legal_dates_signature: .legal_dates_signature.value | split("| "),
+manifs_pdf: .manifs_pdf.value | split("| "),
+manifs_html: .manifs_html.value | split("| "),
+pdfs_to_download: .pdfs_to_download.value | split("| "),
+htmls_to_download: .htmls_to_download.value | split("| ")
 }'''
 
 SEARCH_RULE = ".[] | "
@@ -84,87 +98,165 @@ def get_covid19_items():
     minio.empty_bucket(object_name_prefix=RESOURCE_FILE_PREFIX)
     minio.empty_bucket(object_name_prefix=TIKA_FILE_PREFIX)
 
-    query = """prefix cdm: <http://publications.europa.eu/ontology/cdm#>
-    prefix lang: <http://publications.europa.eu/resource/authority/language/>
+    query = """PREFIX cdm: <http://publications.europa.eu/ontology/cdm#>
+    PREFIX lang: <http://publications.europa.eu/resource/authority/language/>
+    PREFIX res_type: <http://publications.europa.eu/resource/authority/resource-type/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
     
-    SELECT
-      DISTINCT ?processed_title 
-      ?date_document
-      ?celex
-      ?full_oj
-      ?manifestation_pdf
-      ?manifestation_html
-      ?pdf_to_download
-      ?html_to_download
-      ?oj_sector
-      ?resource_type
-      ?legal_date_entry_into_force
-      GROUP_CONCAT(DISTINCT ?author; separator=",") as ?authors
-      GROUP_CONCAT(DISTINCT ?eurovoc_concept; separator=", ") as ?eurovoc_concepts
-      GROUP_CONCAT(DISTINCT ?subject_matter; separator=", ") as ?subject_matters
-      GROUP_CONCAT(DISTINCT ?directory_code; separator=", ") as ?directory_codes
-      GROUP_CONCAT(DISTINCT ?legal_eli; separator=", ") as ?legal_elis
+    SELECT DISTINCT
+        ?work ?title
     
-    WHERE
-    {
-      ?work cdm:resource_legal_comment_internal ?comment .
-      
-      FILTER(regex(STR(?comment),'COVID19'))
+        (group_concat(DISTINCT ?cdm_type; separator="| ") as ?cdm_types)
+        (group_concat(DISTINCT ?cdm_type_label; separator="| ") as ?cdm_type_labels)
     
-      ?work cdm:work_date_document ?date_document .
-      ?work cdm:work_created_by_agent ?author . 
-      ?work cdm:resource_legal_id_celex ?celex .
+        (group_concat(DISTINCT ?resource_type; separator="| ") as ?resource_types)
+        (group_concat(DISTINCT ?resource_type_label; separator="| ") as ?resource_type_labels)
     
-      OPTIONAL {
-        ?work cdm:work_is_about_concept_eurovoc ?eurovoc_concept .
-      }
-      OPTIONAL {
-        ?work cdm:resource_legal_is_about_subject-matter ?subject_matter .
-      }
-      OPTIONAL {
-        ?work cdm:resource_legal_is_about_concept_directory-code ?directory_code .
-      }
-      OPTIONAL {
-        ?work cdm:resource_legal_date_entry-into-force ?legal_date_entry_into_force .
-      }
-      OPTIONAL {
-        ?work cdm:resource_legal_eli ?legal_eli .
-      }
+        (group_concat(DISTINCT ?eurovoc_concept; separator="| ") as ?eurovoc_concepts)
+        (group_concat(DISTINCT ?eurovoc_concept_label; separator="| ") as ?eurovoc_concept_labels)
     
-      ?work cdm:resource_legal_id_sector ?oj_sector .
+        (group_concat(DISTINCT ?subject_matter; separator="| ") as ?subject_matters)
+        (group_concat(DISTINCT ?subject_matter_label; separator="| ") as ?subject_matter_labels)
     
-      OPTIONAL {
-        ?work cdm:work_has_resource-type ?resource_type .
-      }
+        (group_concat(DISTINCT ?directory_code; separator="| ") as ?directory_codes)
+        (group_concat(DISTINCT ?directory_code_label; separator="| ") as ?directory_codes_labels)
     
-      OPTIONAL {
-        ?work cdm:resource_legal_published_in_official-journal ?full_oj .
-      }
+        (group_concat(DISTINCT ?celex; separator="| ") as ?celex_numbers)
+        (group_concat(DISTINCT ?legal_eli; separator="| ") as ?legal_elis)
+        (group_concat(DISTINCT ?id_document; separator="| ") as ?id_documents)
+        (group_concat(DISTINCT ?same_as_uri; separator="| ") as ?same_as_uris)
     
-      OPTIONAL {
-        ?exp cdm:expression_title ?title .
-        ?exp cdm:expression_uses_language ?lang .
-        ?exp cdm:expression_belongs_to_work ?work .
+        (group_concat(DISTINCT ?author; separator="| ") as ?authors)
+        (group_concat(DISTINCT ?author_label; separator="| ") as ?author_labels)
     
-        FILTER(?lang = lang:ENG)
+        (group_concat(DISTINCT ?full_oj; separator="| ") as ?full_ojs)
+        (group_concat(DISTINCT ?oj_sector; separator="| ") as ?oj_sectors)
+        (group_concat(DISTINCT ?internal_comment; separator="| ") as ?internal_comments)
+        (group_concat(DISTINCT ?in_force; separator="| ") as ?is_in_force)
     
+        (group_concat(DISTINCT ?date_document; separator="| ") as ?dates_document)
+        (group_concat(DISTINCT ?date_created; separator="| ") as ?dates_created)
+        (group_concat(DISTINCT ?legal_date_entry_into_force; separator="| ") as ?legal_dates_entry_into_force)
+        (group_concat(DISTINCT ?legal_date_signature; separator="| ") as ?legal_dates_signature)
+    
+        (group_concat(DISTINCT ?manif_pdf; separator="| ") as ?manifs_pdf)
+        (group_concat(DISTINCT ?manif_html; separator="| ") as ?manifs_html)
+        (group_concat(DISTINCT ?pdf_to_download; separator="| ") as ?pdfs_to_download)
+        (group_concat(DISTINCT ?html_to_download; separator="| ") as ?htmls_to_download)
+    WHERE {
+        # selector criteria
+        VALUES ?expr_lang { lang:ENG}
+        ?work cdm:resource_legal_comment_internal ?comment .
+        FILTER (regex(str(?comment),'COVID19'))
+        FILTER NOT EXISTS { ?work cdm:work_embargo [] . }
+    
+        # metadata - classification criteria
         OPTIONAL {
-          ?manifestation_pdf cdm:manifestation_manifests_expression ?exp .
-          ?manifestation_pdf cdm:manifestation_type ?type_pdf .
-          FILTER(STR(?type_pdf) in ('pdf', 'pdfa1a', 'pdfa2a', 'pdfa1b', 'pdfx')) 
+            ?work a ?cdm_type .
+            OPTIONAL {
+                ?cdm_type skos:prefLabel ?cdm_type_label .
+                FILTER (lang(?cdm_type_label)="en")
+            }
+        }
+        OPTIONAL {
+            ?work cdm:work_has_resource-type ?resource_type .
+            OPTIONAL {
+                ?resource_type skos:prefLabel ?resource_type_label .
+                FILTER (lang(?resource_type_label)="en")
+            }
+        }
+        OPTIONAL {
+            ?work cdm:resource_legal_is_about_subject-matter ?subject_matter .
+            OPTIONAL {
+                ?subject_matter skos:prefLabel ?subject_matter_label .
+                FILTER (lang(?subject_matter_label)="en")
+            }
+        }
+        OPTIONAL {
+            ?work cdm:resource_legal_is_about_concept_directory-code ?directory_code .
+            OPTIONAL {
+                ?directory_code skos:prefLabel ?directory_code_label .
+                FILTER (lang(?directory_code_label)="en")
+            }
+        }
+        OPTIONAL {
+            ?work cdm:work_is_about_concept_eurovoc ?eurovoc_concept .
+            OPTIONAL {
+                ?eurovoc_concept skos:prefLabel ?eurovoc_concept_label .
+                FILTER (lang(?eurovoc_concept_label)="en")
+            }
         }
     
+        # metadata - descriptive criteria
         OPTIONAL {
-          ?manifestation_html cdm:manifestation_manifests_expression ?exp .
-          ?manifestation_html cdm:manifestation_type ?type_html .
-          FILTER(STR(?type_html) in ('html', 'xhtml'))
+            ?work cdm:work_created_by_agent ?author .
+            OPTIONAL {
+                ?author skos:prefLabel ?author_label .
+                FILTER (lang(?author_label)="en")
+            }
         }
-      }
-      BIND(IF(BOUND(?title), ?title, 'The title does not exist in that language'@en) as ?processed_title)
-      BIND(IRI(CONCAT(?manifestation_pdf,"/zip")) as ?pdf_to_download)
-      BIND(IRI(CONCAT(?manifestation_html,"/zip")) as ?html_to_download)
+        OPTIONAL {
+            ?work cdm:resource_legal_in-force ?in_force .
+        }
+        OPTIONAL {
+            ?work cdm:resource_legal_id_sector ?oj_sector .
+        }
+        OPTIONAL {
+            ?work cdm:resource_legal_published_in_official-journal ?full_oj .
+        }
+        OPTIONAL {
+            ?work cdm:resource_legal_comment_internal ?internal_comment .
+        }
+        OPTIONAL {
+            ?work cdm:work_date_document ?date_document .
+        }
+        OPTIONAL {
+            ?work cdm:work_date_creation ?date_created .
+        }
+        OPTIONAL {
+            ?work cdm:resource_legal_date_signature ?legal_date_signature .
+        }
+        OPTIONAL {
+            ?work cdm:resource_legal_date_entry-into-force ?legal_date_entry_into_force .
+        }
     
-    } ORDER by ?date_document"""
+        # metadata - identification properties
+        OPTIONAL {
+            ?work cdm:resource_legal_id_celex ?celex .
+        }
+        OPTIONAL {
+            ?work cdm:resource_legal_eli ?legal_eli .
+        }
+        OPTIONAL {
+            ?work cdm:work_id_document ?id_document .
+        }
+        OPTIONAL {
+            ?work owl:sameAs ?same_as_uri .
+        }
+    
+        # diving down FRBR structure for the title and the content
+        OPTIONAL {
+            ?exp cdm:expression_belongs_to_work ?work ;
+                 cdm:expression_uses_language ?expr_lang ;
+                 cdm:expression_title ?title .
+    
+            OPTIONAL {
+                ?manif_pdf cdm:manifestation_manifests_expression ?exp ;
+                           cdm:manifestation_type ?type_pdf .
+                FILTER (str(?type_pdf) in ('pdf', 'pdfa1a', 'pdfa2a', 'pdfa1b', 'pdfx'))
+            }
+            OPTIONAL {
+                ?manif_html cdm:manifestation_manifests_expression ?exp ;
+                            cdm:manifestation_type ?type_html .
+                FILTER (str(?type_html) in ('html', 'xhtml'))
+            }
+        }
+        BIND(IRI(concat(?manif_pdf,"/zip")) as ?pdf_to_download)
+        BIND(IRI(concat(?manif_html,"/zip")) as ?html_to_download)
+    }
+    GROUP BY ?work ?title
+    ORDER BY ?work ?title"""
 
     response = make_request(query)['results']['bindings']
     transformed_json = compile(get_transformation_rules(transformation)).input(response).all()
@@ -180,7 +272,7 @@ def download_file(source: dict, location_details: str, file_name: str, minio: Mi
             else 'http://' + location_details
         request = requests.get(url, allow_redirects=True, timeout=30)
         minio.put_object(RESOURCE_FILE_PREFIX + file_name, request.content)
-        source[CONTENT_PATH_KEY] = file_name
+        source[CONTENT_PATH_KEY].append(file_name)
         return True
 
     except Exception as e:
@@ -203,24 +295,28 @@ def download_covid19_items():
     }
 
     for index, item in enumerate(eurlex_json):
-        if item.get('manifestation_html'):
-            filename = hashlib.sha256(item['manifestation_html'].encode('utf-8')).hexdigest()
+        item[CONTENT_PATH_KEY] = list()
+        if item.get('manifs_html'):
+            for html_manifestation in item.get('htmls_to_download'):
+                filename = hashlib.sha256(html_manifestation.encode('utf-8')).hexdigest()
 
-            logger.info(
-                f"[{index + 1}/{eurlex_items_count}] Downloading HTML manifestation for {item['title']}")
+                logger.info(
+                    f"[{index + 1}/{eurlex_items_count}] Downloading HTML manifestation for {item['title']}")
 
-            html_file = filename + '_html.zip'
-            if download_file(item, item['html_to_download'], html_file, minio):
-                counter['html'] += 1
+                html_file = filename + '_html.zip'
+                if download_file(item, html_manifestation, html_file, minio):
+                    counter['html'] += 1
         elif item.get('manifestation_pdf'):
-            filename = hashlib.sha256(item['manifestation_pdf'].encode('utf-8')).hexdigest()
+            for pdf_manifestation in item.get('pdfs_to_download'):
 
-            logger.info(
-                f"[{index + 1}/{eurlex_items_count}] Downloading PDF manifestation for {item['title']}")
+                filename = hashlib.sha256(pdf_manifestation.encode('utf-8')).hexdigest()
 
-            pdf_file = filename + '_pdf.zip'
-            if download_file(item, item['pdf_to_download'], pdf_file, minio):
-                counter['pdf'] += 1
+                logger.info(
+                    f"[{index + 1}/{eurlex_items_count}] Downloading PDF manifestation for {item['title']}")
+
+                pdf_file = filename + '_pdf.zip'
+                if download_file(item, pdf_manifestation, pdf_file, minio):
+                    counter['pdf'] += 1
         else:
             logger.exception(f"No manifestation has been found for {item['title']}")
 
@@ -245,7 +341,7 @@ def extract_document_content_with_tika():
         valid_sources = 0
         identifier = item['title']
         logger.info(f'[{index + 1}/{eurlex_items_count}] Processing {identifier}')
-        item['content'] = list()
+        item[CONTENT_KEY] = list()
 
         if FAILURE_KEY in item:
             logger.info(
@@ -253,32 +349,34 @@ def extract_document_content_with_tika():
         else:
             try:
                 with tempfile.TemporaryDirectory() as temp_dir:
-                    current_zip_location = Path(temp_dir) / Path(item[CONTENT_PATH_KEY])
-                    with open(current_zip_location, 'wb') as current_zip:
-                        content_bytes = bytearray(minio.get_object(RESOURCE_FILE_PREFIX + item[CONTENT_PATH_KEY]))
-                        current_zip.write(content_bytes)
-                    with zipfile.ZipFile(current_zip_location, 'r') as zip_ref:
-                        zip_ref.extractall(temp_dir)
+                    for content_path in item[CONTENT_PATH_KEY]:
+                        current_zip_location = Path(temp_dir) / Path(content_path)
+                        with open(current_zip_location, 'wb') as current_zip:
+                            content_bytes = bytearray(minio.get_object(RESOURCE_FILE_PREFIX + content_path))
+                            current_zip.write(content_bytes)
+                        with zipfile.ZipFile(current_zip_location, 'r') as zip_ref:
+                            zip_ref.extractall(temp_dir)
 
-                    logger.info(f'Processing each file from {item[CONTENT_PATH_KEY]}:')
-                    for content_file in chain(Path(temp_dir).glob('*.html'), Path(temp_dir).glob('*.pdf')):
-                        logger.info(f'Parsing {Path(content_file).name}')
-                        counter['general'] += 1
-                        parse_result = parser.from_file(str(content_file), APACHE_TIKA_URL)
+                        logger.info(f'Processing each file from {content_path}:')
+                        for content_file in chain(Path(temp_dir).glob('*.html'), Path(temp_dir).glob('*.pdf')):
+                            logger.info(f'Parsing {Path(content_file).name}')
+                            counter['general'] += 1
+                            parse_result = parser.from_file(str(content_file), APACHE_TIKA_URL)
 
-                        if 'content' in parse_result:
-                            logger.info(f'Parse result (first 100 characters): {dumps(parse_result)[:100]}')
-                            item['content'].append(parse_result['content'])
-                            counter['success'] += 1
+                            if 'content' in parse_result:
+                                logger.info(f'Parse result (first 100 characters): {dumps(parse_result)[:100]}')
+                                item[CONTENT_KEY].append(parse_result['content'])
+                                counter['success'] += 1
 
-                            valid_sources += 1
-                        else:
-                            logger.warning(
-                                f'Apache Tika did NOT return a valid content for the source {Path(content_file).name}')
+                                valid_sources += 1
+                            else:
+                                logger.warning(
+                                    f'Apache Tika did NOT return a valid content for the source {Path(content_file).name}')
             except Exception as e:
                 logger.exception(e)
         if valid_sources:
-            filename = hashlib.sha256(item['manifestation_html'].encode('utf-8')).hexdigest()
+            manifestation = item.get('manifs_html', [None])[0] or item.get('manifs_pdf', [None])[0]
+            filename = hashlib.sha256(manifestation.encode('utf-8')).hexdigest()
             minio.put_object_from_string(TIKA_FILE_PREFIX + filename, dumps(item))
 
     minio.put_object_from_string(EURLEX_JSON, dumps(eurlex_json))
