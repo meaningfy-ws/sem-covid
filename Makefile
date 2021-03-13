d131737d1b732fa67f12f308080b3c4d73a9b06d
Mihai Coslet <coslet.mihai@gmail.com>
2021-03-13 13:34:55 +0200

    Extract links, press contacts and detail content.
    - add requirements.txt for scrapy project
    - add splash container for JS rendering

--- Makefile
+++ Makefile
@@ -9,10 +9,20 @@ install:
 	@ pip install --upgrade pip
 	@ pip install -r resources/requirements.txt
 
+install-scrapy-dependencies:
+	@ echo "$(BUILD_PRINT)Installing Scrapy the requirements"
+	@ pip install --upgrade pip
+	@ pip install -r eu_action_timeline/requirements.txt
+
+start-splash:
+	@ echo -e '$(BUILD_PRINT)(dev) Starting the splash container'
+	@ docker-compose --file eu_action_timeline/docker-compose.yml up -d splash
+
 create-indexes:
 	@ echo "$(BUILD_PRINT)Creating indexes and their respective mappings"
 	@ curl -X PUT "http://localhost:9200/pwdb-index" -H 'Content-Type: application/json' -H "Authorization: Basic ZWxhc3RpYzpjaGFuZ2VtZQ==" -d @resources/elasticsearch/pwdb_index_mapping.json
 	@ curl -X PUT "http://localhost:9200/eurlex-index" -H 'Content-Type: application/json' -H "Authorization: Basic ZWxhc3RpYzpjaGFuZ2VtZQ==" -d @resources/elasticsearch/eurlex_index_mapping.json
+	@ curl -X PUT "http://localhost:9200/legal-initiatives-index" -H 'Content-Type: application/json' -H "Authorization: Basic ZWxhc3RpYzpjaGFuZ2VtZQ==" -d @resources/elasticsearch/legal_initiatives_mapping.json
 	@ curl -X PUT "http://localhost:9200/treaties-index" -H 'Content-Type: application/json' -H "Authorization: Basic ZWxhc3RpYzpjaGFuZ2VtZQ==" -d @resources/elasticsearch/treaties_index_mapping.json
 
 all: install
--- eu_action_timeline/eu_action_timeline/spiders/eu_timeline_spider.py
+++ eu_action_timeline/eu_action_timeline/spiders/eu_timeline_spider.py
@@ -1,6 +1,7 @@
 import scrapy
+from scrapy_splash import SplashRequest
+
 from ..items import EuActionTimelineItem
-from scrapy_selenium import SeleniumRequest
 
 
 class EUTimelineSpider(scrapy.Spider):
@@ -9,47 +10,31 @@ class EUTimelineSpider(scrapy.Spider):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
     }
-    json_file = 'parsed.json'
-
-    def process_request(self, request, spider):
-        driver = webdriver.PhantomJS()
-        driver.get(request.url)
-
-        body = driver.page_source
-        return SeleniumRequest(driver.current_url, body=body, encoding='utf-8', request=request)
 
     def start_requests(self):
         yield scrapy.Request(url=self.url, callback=self.parse_main_page, headers=self.headers)
 
-    def parse_main_page(self, response, **kwargs):
+    def parse_main_page(self, response):
         data = {}
         timeline_data = response.xpath(
             '//div[@class="field field-name-field-core-timelines field--field-core-timelines"]/div["field__items"]/*[ @class !="clearfix"]')
         month_name = ''
-        for index, block in enumerate(timeline_data[:2]):
+        for index, block in enumerate(timeline_data):
             if not index % 2:
                 self.logger.info(f'Processing data for {month_name}.')
 
                 month_name = block.xpath('*//h2/text()').get()
-                data[month_name] = list()
 
             else:
                 month_timeline = block.xpath('*//li')
                 for month in month_timeline:
                     date = month.xpath('*[@class="timeline__list__item__title"]/text()').get()
-                    title = month.xpath('*//h4/text()').get()
+                    title = month.xpath('*//h4/text() | *//h4/strong/text()').get()
                     body = ' '.join(month.xpath('*//p[string-length(text()) > 0]').extract())
 
-                    base_url = 'https://ec.europa.eu/commission/presscorner/detail'
+                    presscorner_base_url = 'https://ec.europa.eu/commission/presscorner/detail'
                     presscorner_links = [link.attrib['href'] for link in month.xpath('*//p//a') if
-                                         base_url in link.attrib['href']]
-                    data[month_name].append({
-                        'date': str(date),
-                        'title': str(title),
-                        'excerpt': str(body),
-                        'presscorner_links': presscorner_links,
-                        'links': [link.attrib['href'] for link in month.xpath('*//p//a')]
-                    })
+                                         presscorner_base_url in link.attrib['href']]
 
                     for presscorner_link in presscorner_links:
                         self.logger.info(f'Processing data for this link: {presscorner_link}.')
@@ -60,14 +45,11 @@ class EUTimelineSpider(scrapy.Spider):
                         meta['excerpt'] = body
                         meta['presscorner_links'] = presscorner_links
                         meta['all_links'] = [link.attrib['href'] for link in month.xpath('*//p//a')]
-                        yield SeleniumRequest(url=presscorner_link, callback=self.parse_presscorner_page,
-                                              headers=self.headers,
-                                              wait_time=25,
-                                              meta=meta)
+                        yield SplashRequest(url=presscorner_link, callback=self.parse_presscorner_page,
+                                            args={'wait': 5},
+                                            meta=meta)
 
-    def parse_presscorner_page(self, response, **kwargs):
-        import time
-        time.sleep(5)
+    def parse_presscorner_page(self, response):
         meta = response.meta
         item = EuActionTimelineItem()
         item['month_name'] = meta['month_name']
@@ -76,7 +58,24 @@ class EUTimelineSpider(scrapy.Spider):
         item['excerpt'] = meta['excerpt']
         item['presscorner_links'] = meta['presscorner_links']
         item['all_links'] = meta['all_links']
-        item['detail_body'] = response.body.decode()
+        item['article_content'] = response.xpath('//div[@class="ecl-paragraph"]').get()
         item['detail_title'] = response.xpath(
             '//h1[@class="ecl-heading ecl-heading--h1 ecl-u-color-white"]//text()').extract()
+
+        detail_links_start = response.xpath('//p[contains(., "For More Information")]')
+        if detail_links_start:
+            item['for_more_information_links'] = [link.attrib['href'] for link in
+                                                  detail_links_start[0].xpath('following-sibling::p/a')]
+        item['detail_pdf_link'] = response.xpath('//a[contains(@class, "ecl-button--file ecl-file__download")]').attrib[
+            'href']
+
+        item['press_contacts'] = list()
+        press_contacts = response.xpath('//ul[@class="ecl-listing"]/li')
+        for press_contact in press_contacts:
+            item['press_contacts'].append({
+                'name': press_contact.xpath('*//div/h4/text()').get(),
+                'phone': press_contact.xpath('*//div/div[@class="ecl-field__body"]/text()').get(),
+                'email': press_contact.xpath('*//div/div[@class="ecl-field__body"]/a/text()').get()
+            })
+
         yield item
