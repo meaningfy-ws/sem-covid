4060169976491f69a5ab339a7edf5558ecdf931c
Mihai Coslet <coslet.mihai@gmail.com>
2021-03-16 12:38:34 +0200

    update naming and other PR comments

--- eu_action_timeline/eu_action_timeline/spiders/eu_timeline_spider.py
+++ eu_action_timeline/eu_action_timeline/spiders/eu_timeline_spider.py
@@ -7,6 +7,7 @@ from ..items import EuActionTimelineItem
 class EUTimelineSpider(scrapy.Spider):
     name = 'eu-timeline'
     url = 'https://ec.europa.eu/info/live-work-travel-eu/coronavirus-response/timeline-eu-action_en'
+    presscorner_base_url = 'https://ec.europa.eu/commission/presscorner/detail'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
     }
@@ -29,45 +30,48 @@ class EUTimelineSpider(scrapy.Spider):
                 month_timeline = block.xpath('*//li')
                 for month in month_timeline:
                     date = month.xpath('*[@class="timeline__list__item__title"]/text()').get()
-                    title = month.xpath('*//h4/text() | *//h4/strong/text()').get()
+                    title = month.xpath('*//h4//text()').get()
                     body = ' '.join(month.xpath('*//p[string-length(text()) > 0]').extract())
 
-                    presscorner_base_url = 'https://ec.europa.eu/commission/presscorner/detail'
                     presscorner_links = [link.attrib['href'] for link in month.xpath('*//p//a') if
-                                         presscorner_base_url in link.attrib['href']]
-
-                    for presscorner_link in presscorner_links:
-                        self.logger.info(f'Processing data for this link: {presscorner_link}.')
-                        meta = {}
-                        meta['month_name'] = month_name
-                        meta['date'] = date
-                        meta['title'] = title
-                        meta['excerpt'] = body
-                        meta['presscorner_links'] = presscorner_links
-                        meta['all_links'] = [link.attrib['href'] for link in month.xpath('*//p//a')]
-                        yield SplashRequest(url=presscorner_link, callback=self.parse_presscorner_page,
-                                            args={'wait': 5},
-                                            meta=meta)
+                                         self.presscorner_base_url in link.attrib.get('href', '')]
+                    meta = dict()
+                    meta['month_name'] = month_name
+                    meta['date'] = date
+                    meta['title'] = title
+                    meta['excerpt'] = body
+                    meta['presscorner_links'] = presscorner_links
+                    meta['all_links'] = [link.attrib['href'] for link in month.xpath('*//p//a')]
+                    if presscorner_links:
+                        for presscorner_link in presscorner_links:
+                            self.logger.info(f'Processing data for this link: {presscorner_link}.')
+                            yield SplashRequest(url=presscorner_link, callback=self.parse_presscorner_page,
+                                                args={'wait': 5 },
+                                                meta=meta)
+                    else:
+                        yield EuActionTimelineItem(**meta)
 
     def parse_presscorner_page(self, response):
         meta = response.meta
-        item = EuActionTimelineItem()
-        item['month_name'] = meta['month_name']
-        item['date'] = meta['date']
-        item['title'] = meta['title']
-        item['excerpt'] = meta['excerpt']
-        item['presscorner_links'] = meta['presscorner_links']
-        item['all_links'] = meta['all_links']
+        item = EuActionTimelineItem(
+            month_name=meta['month_name'],
+            date=meta['date'],
+            title=meta['title'],
+            excerpt=meta['excerpt'],
+            presscorner_links=meta['presscorner_links'],
+            all_links=meta['all_links']
+        )
         item['article_content'] = response.xpath('//div[@class="ecl-paragraph"]').get()
         item['detail_title'] = response.xpath(
             '//h1[@class="ecl-heading ecl-heading--h1 ecl-u-color-white"]//text()').extract()
 
         detail_links_start = response.xpath('//p[contains(., "For More Information")]')
         if detail_links_start:
-            item['for_more_information_links'] = [link.attrib['href'] for link in
+            item['for_more_information_links'] = [link.attrib.get('href') for link in
                                                   detail_links_start[0].xpath('following-sibling::p/a')]
-        item['detail_pdf_link'] = response.xpath('//a[contains(@class, "ecl-button--file ecl-file__download")]').attrib[
-            'href']
+        item['detail_pdf_link'] = response.xpath(
+            '//a[contains(@class, "ecl-button--file ecl-file__download")]').attrib.get(
+            'href')
 
         item['press_contacts'] = list()
         press_contacts = response.xpath('//ul[@class="ecl-listing"]/li')
