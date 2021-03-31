5cf9aec4305a5b3c83140b191f9992558ce9c991
DanCh11 <dan.chiriac1453@gmail.com>
2021-03-31 13:19:51 +0300

    pwdb experiments dags

---
+++ ml_experiments/entrypoints/dags/pwdb_base_experiment_dag.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# dummy_experiment_dag.py
+# Date:  29/03/2021
+# Author: Dan Chiriac
+# Email: dan.chiriac1453@gmail.com
+
+""" """
+
+from datetime import datetime
+
+from ml_experiments.services.pwdb_base_experiment import PWDBBaseExperiment
+
+dag = PWDBBaseExperiment().create_dag(start_date=datetime.now())
+globals()[dag.dag_id] = dag
