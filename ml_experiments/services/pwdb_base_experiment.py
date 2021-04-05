8df2e2f7f5d4f981c355bdd55cd958fdc10a1aac
DanCh11 <dan.chiriac1453@gmail.com>
2021-04-05 14:14:32 +0300

    improved pwdb base experiment methods

--- ml_experiments/services/pwdb_base_experiment.py
+++ ml_experiments/services/pwdb_base_experiment.py
@@ -11,78 +11,88 @@
 """
 
 import logging
-import requests
 import pickle
 from abc import ABC
 from json import loads, dumps
 
 from jq import compile
 import pandas as pd
+from pandas import DataFrame
 from gensim.models import Word2Vec
 from sklearn.model_selection import train_test_split
 
+from ml_experiments.config import config
 from ml_experiments.services.sc_wrangling.pwdb_transformer import get_transformation_rules
-from ml_experiments.services.sc_wrangling.feature_selector import (reduce_array_column,
-                                                                   multi_label_column_to_binary_columns)
+from ml_experiments.services.sc_wrangling.feature_selector import reduce_array_column
 from ml_experiments.services.sc_wrangling.data_cleaning import prepare_text_for_cleaning
 from ml_experiments.services.sc_wrangling.value_replacement import MultiColumnLabelEncoder
-from ml_experiments.adapters.minio_adapter import MinioAdapter
 from ml_experiments.services.base_experiment import BaseExperiment
 
 logger = logging.getLogger(__name__)
 
-ML_EXPERIMENTS_BUCKET_NAME = "ml-experiments"
-SC_COVID_JSON = "covid19.json"
-SC_COVID_DATASET = "pwdb_dataset.pkl"
+BUSINESSES = [
+    'Companies providing essential services', 'Contractors of a company',
+    'Larger corporations', 'One person or microenterprises', 'Other businesses',
+    'SMEs', 'Sector specific set of companies', 'Solo-self-employed', 'Start-ups']
 
-PWDB_DATASET_URL = 'http://static.eurofound.europa.eu/covid19db/data/covid19db.json'
-MINIO_ACCESS_KEY = '2zVld17bTfKk8iu0Eh9H74MywAeDV3WQ'
-MINIO_SECRET_KEY = 'ddk9fixfI9qXiMaZu1p2a7BsgY2yDopm'
-MINIO_URL = 'srv.meaningfy.ws:9000'
+CITIZENS = [
+    'Children (minors)', 'Disabled', 'Migrants', 'Older citizens',
+    'Other groups of citizens', 'Parents', 'People in care facilities', 'Refugees',
+    'Single parents', 'The COVID-19 risk group', 'Women', 'Youth (18-25)']
 
-transformation = '''{
-"Identifier": .recordId,
-"Title": .fieldData.title,
-"Title (national language)": .fieldData.title_nationalLanguage,
-"Country": .fieldData.calc_country,
-"Start date": .fieldData.d_startDate,
-"End date": .fieldData.d_endDate,
-"Date type": .fieldData.dateType,
-"Type of measure": .fieldData.calc_type,
-"Status of regulation": .fieldData.statusOfRegulation,
-"Category": .fieldData.calc_minorCategory,
-"Subcategory": .fieldData.calc_subMinorCategory,
-"Case added": .fieldData.calc_creationDay,
-"Background information": .fieldData.descriptionBackgroundInfo,
-"Content of measure": .fieldData.descriptionContentOfMeasure,
-"Use of measure": .fieldData.descriptionUseOfMeasure,
-"Actors": [.portalData.actors[] |  ."actors::name" ],
-"Target groups": [.portalData.targetGroups[] | ."targetGroups::name"],
-"Funding": [.portalData.funding[] | ."funding::name" ],
-"Views of social partners": .fieldData.descriptionInvolvementOfSocialPartners,
-"Form of social partner involvement": .fieldData.socialPartnerform,
-"Role of social partners": .fieldData.socialPartnerrole,
-"Is sector specific": .fieldData.isSector,
-"Private or public sector": .fieldData.sector_privateOrPublic,
-"Is occupation specific": .fieldData.isOccupation,
-"Sectors": [.portalData.sectors[] | ."sectors::name" ],
-"Occupations": [.portalData.occupations[] | .],
-"Sources": [.portalData.sources[] | ."sources::url" ],
+WORKERS = [
+    'Cross-border commuters', 'Disabled workers', 'Employees in standard employment',
+    'Female workers', 'Migrants in employment', 'Older people in employment (aged 55+)',
+    'Other groups of workers', 'Parents in employment', 'Particular professions',
+    'Platform workers', 'Posted workers', 'Refugees in employment', 'Seasonal workers',
+    'Self-employed', 'Single parents in employment', 'The COVID-19 risk group at the workplace',
+    'Undeclared workers', 'Unemployed', 'Workers in care facilities',
+    'Workers in essential services', 'Workers in non-standard forms of employment',
+    'Youth (18-25) in employment']
+
+PWDB_REFACTORING_RULES = '''{
+    "Identifier": .recordId,
+    "Title": .fieldData.title,
+    "Title (national language)": .fieldData.title_nationalLanguage,
+    "Country": .fieldData.calc_country,
+    "Start date": .fieldData.d_startDate,
+    "End date": .fieldData.d_endDate,
+    "Date type": .fieldData.dateType,
+    "Type of measure": .fieldData.calc_type,
+    "Status of regulation": .fieldData.statusOfRegulation,
+    "Category": .fieldData.calc_minorCategory,
+    "Subcategory": .fieldData.calc_subMinorCategory,
+    "Case added": .fieldData.calc_creationDay,
+    "Background information": .fieldData.descriptionBackgroundInfo,
+    "Content of measure": .fieldData.descriptionContentOfMeasure,
+    "Use of measure": .fieldData.descriptionUseOfMeasure,
+    "Actors": [.portalData.actors[] |  ."actors::name" ],
+    "Target groups": [.portalData.targetGroups[] | ."targetGroups::name"],
+    "Funding": [.portalData.funding[] | ."funding::name" ],
+    "Views of social partners": .fieldData.descriptionInvolvementOfSocialPartners,
+    "Form of social partner involvement": .fieldData.socialPartnerform,
+    "Role of social partners": .fieldData.socialPartnerrole,
+    "Is sector specific": .fieldData.isSector,
+    "Private or public sector": .fieldData.sector_privateOrPublic,
+    "Is occupation specific": .fieldData.isOccupation,
+    "Sectors": [.portalData.sectors[] | ."sectors::name" ],
+    "Occupations": [.portalData.occupations[] | .],
+    "Sources": [.portalData.sources[] | ."sources::url" ],
 }'''
 
 
 class PWDBBaseExperiment(BaseExperiment):
 
     def data_extraction(self, *args, **kwargs):
-        response = requests.get(PWDB_DATASET_URL, stream=True, timeout=30)
-        response.raise_for_status()
-        minio = MinioAdapter(MINIO_URL, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, ML_EXPERIMENTS_BUCKET_NAME)
-        minio.empty_bucket()
-        transformed_json = compile(get_transformation_rules(transformation)).input(loads(response.content)).all()
-        minio.put_object(SC_COVID_JSON, dumps(transformed_json).encode('utf-8'))
-        dataframe_from_json = pd.DataFrame.from_records(transformed_json)
-        convert_to_pickle = pickle.dumps(dataframe_from_json)
-        minio.put_object("pwdb_dataset.pkl", convert_to_pickle)
+        raw_pwdb_dataset = self.requests.get(config.PWDB_DATASET_URL, stream=True, timeout=30)
+        raw_pwdb_dataset.raise_for_status()
+        self.minio_adapter.empty_bucket()
+        pwdb_json_dataset = compile(get_transformation_rules(PWDB_REFACTORING_RULES)) \
+            .input(loads(raw_pwdb_dataset.content)).all()
+        self.minio_adapter.put_object(config.SC_PWDB_JSON, dumps(pwdb_json_dataset).encode('utf-8'))
+        pwdb_dataframe = pd.DataFrame.from_records(pwdb_json_dataset)
+        pickle_pwdb_dataframe = pickle.dumps(pwdb_dataframe)
+        self.minio_adapter.put_object("pwdb_dataframe.pkl", pickle_pwdb_dataframe)
 
     def data_validation(self, *args, **kwargs):
         # TODO: implement me by validating the returned index structure for a start,
@@ -90,59 +100,14 @@ class PWDBBaseExperiment(BaseExperiment):
         raise NotImplementedError
 
     def data_preparation(self, *args, **kwargs):
-        # Connect to MinIO server
-        minio = MinioAdapter(MINIO_URL, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, ML_EXPERIMENTS_BUCKET_NAME)
-        dataframe = pickle.loads(minio.get_object(SC_COVID_DATASET))
-        # Begin preparation
-        reducer = reduce_array_column(dataframe, "Target groups")
-        df = reducer[['Title', 'Background information', 'Content of measure',
-                      'Category', 'Subcategory', 'Type of measure', 'Target groups']]
-        # Prepare independent data
-        df_description_data = df['Title'].map(str) + df['Background information'].map(str) + \
-                              df['Content of measure'].map(str)
-        concatenate_df = pd.DataFrame(df_description_data, columns=['Concatenated Data'])
-        df['Concatenated Data'] = concatenate_df
-        df['Concatenated Data (clean)'] = df['Concatenated Data'].apply(lambda x: prepare_text_for_cleaning(x))
-        # Prepare label data
-        selected_data = df[['Concatenated Data (clean)', 'Category', 'Subcategory', 'Type of measure', 'Target groups']]
-        label_encoding = MultiColumnLabelEncoder(
-            columns=['Category', 'Subcategory', 'Type of measure']).fit_transform(selected_data)
-        multi_label_target_groups = multi_label_column_to_binary_columns(label_encoding, "Target groups")
-        target_groups_values = multi_label_target_groups['Target groups']
-        businesses = ['Companies providing essential services', 'Contractors of a company',
-                      'Larger corporations', 'One person or microenterprises', 'Other businesses',
-                      'SMEs', 'Sector specific set of companies', 'Solo-self-employed', 'Start-ups']
 
-        citizens = ['Children (minors)', 'Disabled', 'Migrants', 'Older citizens',
-                    'Other groups of citizens', 'Parents', 'People in care facilities', 'Refugees',
-                    'Single parents', 'The COVID-19 risk group', 'Women', 'Youth (18-25)']
-
-        workers = ['Cross-border commuters', 'Disabled workers', 'Employees in standard employment',
-                   'Female workers', 'Migrants in employment', 'Older people in employment (aged 55+)',
-                   'Other groups of workers', 'Parents in employment', 'Particular professions',
-                   'Platform workers', 'Posted workers', 'Refugees in employment', 'Seasonal workers',
-                   'Self-employed', 'Single parents in employment', 'The COVID-19 risk group at the workplace',
-                   'Undeclared workers', 'Unemployed', 'Workers in care facilities',
-                   'Workers in essential services', 'Workers in non-standard forms of employment',
-                   'Youth (18-25) in employment']
-
-        multi_label_target_groups['Businesses'] = target_groups_values.str.contains('|'.join(businesses))
-        multi_label_target_groups['Citizens'] = target_groups_values.str.contains('|'.join(citizens))
-        multi_label_target_groups['Workers'] = target_groups_values.str.contains('|'.join(workers))
-        multi_label_target_groups.replace({True: 1, False: 0}, inplace=True)
-        convert_to_pickle = pickle.dumps(multi_label_target_groups)
-        minio.put_object("pwdb_prepared.pkl", convert_to_pickle)
-        # Split data for train, test and word model training
-        independent_data = multi_label_target_groups['Concatenated Data (clean)']
-        label_data = multi_label_target_groups.drop(['Concatenated Data (clean)', 'Target groups'], axis=1)
-        pwdb_word2vec = Word2Vec(independent_data, window=5, min_count=10, size=300)
-        w2v_saving = pickle.dumps(pwdb_word2vec)
-        minio.put_object("word2vec.gensim.model", w2v_saving)
-        x_train, x_test, y_train, y_test = train_test_split(independent_data, label_data,
-                                                            random_state=42, test_size=0.3, shuffle=True)
-        train_test_dict = {"X_train": x_train, "X_test": x_test, "y_train": y_train, "y_test": y_test}
-        convert_to_pickle = pickle.dumps(train_test_dict)
-        minio.put_object("train_test_split.pkl", convert_to_pickle)
+        pwdb_dataframe = pickle.loads(self.minio_adapter.get_object(config.SC_PWDB_DATA_FRAME))
+        pwdb_dataframe_columns = self.prepare_pwdb_data(pwdb_dataframe)
+        pwdb_word2vec_model = self.train_pwdb_word2vec_language_model(pwdb_dataframe_columns)
+        pwdb_train_test_data = self.train_pwdb_data(pwdb_dataframe_columns)
+        pwdb_word2vec_pickle = pickle.dumps(pwdb_word2vec_model)
+        pwdb_train_test_pickle = pickle.dumps(pwdb_train_test_data)
+        self.minio_adapter.put_object("train_test_split.pkl", pwdb_train_test_pickle)
 
     def model_training(self, *args, **kwargs):
         pass
@@ -153,7 +118,81 @@ class PWDBBaseExperiment(BaseExperiment):
     def model_validation(self, *args, **kwargs):
         pass
 
+    @staticmethod
+    def prepare_pwdb_data(pwdb_dataframe: DataFrame) -> DataFrame:
+        """
+            Before training the model and applying it to different algorithms, we must prepare the data
+            that we extracted. We will work with selected columns which can be viewed down below.
+            First of all we must convert the "Target groups" column from a list, into independent strings.
+            After that we concatenate "Title, Background information, Content of measure" columns,
+            clean them and create separate column named "Descriptive data". As a last step we encode
+            "Category, Subcategory, Type of measure" columns to be prepared for classification labels
+            for Machine Learning algorithms.
 
-pwdb_caller = PWDBBaseExperiment()
-pwdb_caller.data_preparation()
+            :return: As a result, we prepared pwdb dataset with common text data prepared classifier
+                     labels that will be used into train-test part.
+        """
+        reduce_array_column(pwdb_dataframe, "Target groups")
+        pwdb_dataframe_columns = pwdb_dataframe[['Title', 'Background information', 'Content of measure',
+                                                 'Category', 'Subcategory', 'Type of measure', 'Target groups']]
+
+        pwdb_descriptive_data = pwdb_dataframe_columns['Title'].map(str) + ' ' + \
+            pwdb_dataframe_columns['Background information'].map(str) + ' ' + \
+            pwdb_dataframe_columns['Content of measure'].map(str)
+        pwdb_dataframe_columns['Descriptive Data'] = pwdb_descriptive_data.apply(lambda x: prepare_text_for_cleaning(x))
+        pwdb_dataframe_columns = MultiColumnLabelEncoder(
+            columns=['Category', 'Subcategory', 'Type of measure']).fit_transform(pwdb_dataframe_columns)
+
+        return pwdb_dataframe_columns
+
+    @staticmethod
+    def target_group_refactoring(pwdb_dataframe: DataFrame, target_group_column_name: str = 'Target groups') -> DataFrame:
+        """
+            The target group available in the original dataset is very granular. For the purpose of this exercise
+            we would benefit from aggregating the target groups into a more generic sets. As a result we will obtain
+            target groups on two levels: L1, L2.
+            L1: workers, businesses, citizens
+            L2: the original set of categories
+
+            :return: the given dataset with an extra column containing the aggregated (L1) values
+        """
+
+        refactored_pwdb_df = pwdb_dataframe[target_group_column_name]
+        pwdb_dataframe['Businesses'] = refactored_pwdb_df.str.contains('|'.join(BUSINESSES))
+        pwdb_dataframe['Citizens'] = refactored_pwdb_df.str.contains('|'.join(CITIZENS))
+        pwdb_dataframe['Workers'] = refactored_pwdb_df.str.contains('|'.join(WORKERS))
+        refactored_pwdb_df = pd.get_dummies(pwdb_dataframe, columns=[target_group_column_name])
+        refactored_pwdb_df.replace({True: 1, False: 0}, inplace=True)
+
+        return refactored_pwdb_df
+
+    @staticmethod
+    def train_pwdb_word2vec_language_model(pwdb_dataframe: DataFrame) -> Word2Vec:
+        """
+            As language model data it will be use "Descriptive Data" column
+            and will be transform into word2vec model.
+        """
+        pwdb_word2vec = Word2Vec(pwdb_dataframe["Descriptive Data"])
+
+        return pwdb_word2vec
+
+    @staticmethod
+    def train_pwdb_data(pwdb_dataframe: DataFrame) -> dict:
+        """
+            After data preparation step, we have to split existent data into training and testing size.
+            The inputs will be "Descriptive data" and "Category, Subcategory, Type of measure,
+            Target groups L1 and Target groups L2" columns.
+
+            :return: As a result, we will have a dictionary with split data.
+        """
+        pwdb_common_text = pwdb_dataframe['Descriptive Data']
+        pwdb_classifiers = pwdb_dataframe.drop(['Descriptive Data', 'Title',
+                                                'Background information', 'Content of measure'], axis=1)
+        # pwdb_word2vec = Word2Vec(pwdb_common_text, window=5, min_count=10, size=300)
+        x_train, x_test, y_train, y_test = train_test_split(pwdb_common_text, pwdb_classifiers,
+                                                            random_state=42, test_size=0.3, shuffle=True)
+        train_test_dict = {"X_train": x_train, "X_test": x_test, "y_train": y_train, "y_test": y_test}
+
+        return train_test_dict
+
 
