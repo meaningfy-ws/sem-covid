8e707cde849e15f8a2918c9310ad030e528f6460
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2021-04-06 21:40:13 +0300

    finish clean-text warping + unit tests

--- ml_experiments/services/sc_wrangling/data_cleaning.py
+++ ml_experiments/services/sc_wrangling/data_cleaning.py
@@ -1,10 +1,47 @@
 import re
 import string
 
-import nltk
-from nltk.corpus import stopwords
-nltk.download('stopwords')
+from cleantext import clean
+import spacy
 
+nlp = spacy.load("en_core_web_sm")
+
+def clean_fix_unicode(text : str)->str:
+    return clean(text,fix_unicode=True,to_ascii=False,lower=False)
+
+def clean_to_ascii(text : str)->str:
+    return clean(text,to_ascii=True,fix_unicode=False,lower=False)
+
+def clean_to_lower(text : str)->str:
+    return clean(text,fix_unicode=False,to_ascii=False,lower=True)
+
+def clean_remove_line_breaks(text : str)->str:
+    return clean(text,fix_unicode=False,to_ascii=False,lower=False,no_line_breaks=True)
+
+def clean_remove_urls(text : str, replace_with : str = "<URL>")->str:
+    return clean(text, fix_unicode=False, to_ascii=False, lower=False, no_urls=True,replace_with_url=replace_with)
+
+def clean_remove_emails(text : str, replace_with : str = "<EMAIL>")->str:
+    return clean(text, fix_unicode=False, to_ascii=False, lower=False, no_emails=True,replace_with_email=replace_with)
+
+def clean_remove_numbers(text : str, replace_with : str = "<NUMBER>")->str:
+    return clean(text, fix_unicode=False, to_ascii=False, lower=False, no_numbers=True,replace_with_number=replace_with)
+
+def clean_remove_digits(text : str, replace_with : str = "0")->str:
+    return clean(text, fix_unicode=False, to_ascii=False, lower=False, no_digits=True,replace_with_digit=replace_with)
+
+def clean_remove_punct(text : str, replace_with : str = "")->str:
+    return clean(text, fix_unicode=False, to_ascii=False, lower=False, no_punct=True,replace_with_punct=replace_with)
+
+def clean_remove_digits(text : str, replace_with : str = "0")->str:
+    return clean(text, fix_unicode=False, to_ascii=False, lower=False, no_digits=True,replace_with_digit=replace_with)
+
+def clean_remove_currency_symbols(text : str, replace_with : str = "<CUR>")->str:
+    return clean(text, fix_unicode=False, to_ascii=False, lower=False, no_currency_symbols=True,replace_with_currency_symbol=replace_with)
+
+def clean_remove_stopwords(text : str)->str:
+    stop_words = nlp.Defaults.stop_words
+    return " ".join([word for word in text.split() if word not in stop_words])
 
 def prepare_text_for_cleaning(text: str):
     """
@@ -14,7 +51,7 @@ def prepare_text_for_cleaning(text: str):
         symbols, numbers i.e.
         :text: messy string, ready for cleaning
     """
-    stopword = stopwords.words('english')
+    stopword = nlp.Defaults.stop_words
     # set text to lowercase
     text = text.lower()
     # remove links
