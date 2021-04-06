fe55f5271429b9f8970252c39ef8ff4ee54b5b1d
Dan <dan.chiriac1453@gmail.com>
2021-04-06 13:49:16 +0300

    Improved tests for pwdb data preparation

--- tests/unit/test_prepare_pwdb_data.py
+++ tests/unit/test_prepare_pwdb_data.py
@@ -1,36 +1,80 @@
 
-import requests
+import unittest
 
 from pandas import DataFrame
+from pandas.testing import assert_frame_equal, assert_series_equal
 
-from tests.minio_connection import MinioTestFileCreation
+from ml_experiments.config import config
+from tests.minio_connection import MinioFakeConnection
 from ml_experiments.services.pwdb_base_experiment import PWDBBaseExperiment
 
-MINIO_ACCESS_KEY = '2zVld17bTfKk8iu0Eh9H74MywAeDV3WQ'
-MINIO_SECRET_KEY = 'ddk9fixfI9qXiMaZu1p2a7BsgY2yDopm'
-MINIO_URL = 'srv.meaningfy.ws:9000'
-MINIO_TEST_BUCKET = 'test-ml-experiments'
+
+minio = MinioFakeConnection(config.MINIO_URL, config.MINIO_ACCESS_KEY,
+                            config.MINIO_SECRET_KEY, config.MINIO_TEST_BUCKET)
 
 
-def test_prepare_pwdb_data():
-
-    minio = MinioTestFileCreation(MINIO_URL, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, MINIO_TEST_BUCKET)
-    test_dataset = minio.get_test_dataframe_from_minio()
-
-    expected_data = {
+class TestPreparationPWDBData(unittest.TestCase):
+    EXPECTED_DATA = {
         'Title': ['title_one', 'title_two'],
         'Background information': ['bi_one', 'bi_two'],
         'Content of measure': ['cm_one', 'cm_two'],
         'Category': [0, 1],
         'Subcategory': [0, 1],
         'Type of measure': [0, 1],
-        'Target groups': ['tg_l_one|tg_l_two', 'tg_l_one_first|tg_l_two_second'],
+        'Target groups': ['tg_one|tg_two', 'tg_one_first|tg_two_second'],
         'Descriptive Data': [['titleone', 'bione', 'cmone'], ['titletwo', 'bitwo', 'cmtwo']]}
 
-    expected_df = DataFrame(data=expected_data)
+    EXPECTED_DATAFRAME = DataFrame(data=EXPECTED_DATA)
+    TEST_DATAFRAME = minio.get_test_dataframe_from_minio()
+    PWDB_PREPARE_FUNCTION = PWDBBaseExperiment.prepare_pwdb_data(TEST_DATAFRAME)
 
-    pwdb_experiment = PWDBBaseExperiment(minio, requests)
+    def test_len_of_expected_data_and_test_dataframe(self):
+        self.assertEqual(len(self.EXPECTED_DATAFRAME), len(self.PWDB_PREPARE_FUNCTION))
 
-    test_data_preparation = pwdb_experiment.prepare_pwdb_data(test_dataset)
+    def test_compare_expected_data_with_test_dataframe(self):
+        assert_frame_equal(self.EXPECTED_DATAFRAME, self.PWDB_PREPARE_FUNCTION)
 
-    assert len(test_data_preparation) == len(expected_df)
+    def test_descriptive_data_serial_equals_of_expected_and_test_dataframe(self):
+        assert_series_equal(self.EXPECTED_DATAFRAME['Descriptive Data'], self.PWDB_PREPARE_FUNCTION['Descriptive Data'])
+
+    def test_category_equals_of_expected_and_test_dataframe(self):
+        assert_series_equal(self.EXPECTED_DATAFRAME['Category'], self.PWDB_PREPARE_FUNCTION['Category'])
+
+    def test_subcategory_equals_of_expected_and_test_dataframe(self):
+        assert_series_equal(self.EXPECTED_DATAFRAME['Subcategory'], self.PWDB_PREPARE_FUNCTION['Subcategory'])
+
+    def test_type_of_measure_equals_of_expected_and_test_dataframe(self):
+        assert_series_equal(self.EXPECTED_DATAFRAME['Type of measure'], self.PWDB_PREPARE_FUNCTION['Type of measure'])
+
+    def test_target_groups_serial_equals_of_expected_and_test_dataframe(self):
+        assert_series_equal(self.EXPECTED_DATAFRAME['Target groups'], self.PWDB_PREPARE_FUNCTION['Target groups'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
