654b179d8cb4a339f3ebb835e1be02179f4edfdc
Dan <dan.chiriac1453@gmail.com>
2021-04-07 19:36:44 +0300

    replaced function's path

--- tests/unit/test_feature_selector.py
+++ tests/unit/test_feature_selector.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-from sc_wrangling.feature_selector import reduce_array_column, multi_label_column_to_binary_columns
+from ml_experiments.services.sc_wrangling.feature_selector import (reduce_array_column,
+                                                                   multi_label_column_to_binary_columns)
 
 
 def test_reduce_array_column_rows_expectation():
