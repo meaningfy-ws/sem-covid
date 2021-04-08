ebd0662bcb075d15a729f27916b3cfae961cec7a
Dan <dan.chiriac1453@gmail.com>
2021-04-08 11:40:41 +0300

    removed a TODO case and the variable with specific columns, then replaced this variable with all dataset columns. Added new columns in concatenated column.

--- ml_experiments/services/pwdb_base_experiment.py
+++ ml_experiments/services/pwdb_base_experiment.py
@@ -85,7 +85,6 @@ class PWDBBaseExperiment(BaseExperiment, ABC):
         pwdb_train_test_pickle = pickle.dumps(pwdb_train_test_data)
         self.minio_adapter.put_object("train_test_split.pkl", pwdb_train_test_pickle)
 
-
     @staticmethod
     def prepare_pwdb_data(pwdb_dataframe: pd.DataFrame) -> pd.DataFrame:
         """
@@ -102,17 +101,16 @@ class PWDBBaseExperiment(BaseExperiment, ABC):
         """
         feature_selector.reduce_array_column(pwdb_dataframe, "Target groups")
         # TODO: train test split with all cols
-        # TODO: use of measure, involvement social partener, title
-        pwdb_dataframe_columns = pwdb_dataframe[['Title', 'Background information', 'Content of measure',
-                                                 'Category', 'Subcategory', 'Type of measure', 'Target groups']]
+        pwdb_descriptive_data = pwdb_dataframe['Title'].map(str) + ' ' + \
+                                pwdb_dataframe['Background information'].map(str) + ' ' + \
+                                pwdb_dataframe['Content of measure'].map(str) + ' ' + \
+                                pwdb_dataframe['Use of measure'] + ' ' + \
+                                pwdb_dataframe['Views of social partners']
 
-        pwdb_descriptive_data = pwdb_dataframe_columns['Title'].map(str) + ' ' + \
-                                pwdb_dataframe_columns['Background information'].map(str) + ' ' + \
-                                pwdb_dataframe_columns['Content of measure'].map(str)
-        pwdb_dataframe_columns['Descriptive Data'] = pwdb_descriptive_data \
+        pwdb_dataframe['Descriptive Data'] = pwdb_descriptive_data \
             .apply(lambda x: data_cleaning.prepare_text_for_cleaning(x))
         pwdb_dataframe_columns = value_replacement.MultiColumnLabelEncoder(
-            columns=['Category', 'Subcategory', 'Type of measure']).fit_transform(pwdb_dataframe_columns)
+            columns=['Category', 'Subcategory', 'Type of measure']).fit_transform(pwdb_dataframe)
 
         return pwdb_dataframe_columns
 
