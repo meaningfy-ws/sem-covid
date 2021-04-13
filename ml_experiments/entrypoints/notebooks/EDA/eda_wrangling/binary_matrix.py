e7fdf043826450349fe18b05efa89df8eefda7a0
Dan <dan.chiriac1453@gmail.com>
2021-04-13 15:59:00 +0300

    functions for binary converts and dependency table creation

---
+++ ml_experiments/entrypoints/notebooks/EDA/eda_wrangling/binary_matrix.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+
+
+def convert_to_binary_matrix(data: pd.DataFrame) -> pd.DataFrame:
+    """
+     Function to get binary-matrix from DataFrame
+    """
+    binary_matrix = pd.DataFrame([],dtype=object)
+    for index, row in data.iterrows():
+        new_row = {}
+        for key in row.index:
+            if type(row[key]) == list:
+                for column in row[key]:
+                    new_row[column] = 1
+            else:
+                new_row[row[key]] = 1
+        binary_matrix = binary_matrix.append(new_row, ignore_index=True)
+    binary_matrix = binary_matrix.fillna(0)
+    return binary_matrix
+
+
+def dependency_table(data: pd.DataFrame, dependency_level: float = 0.9) -> pd.DataFrame:
+    """
+    Function to get dependency between columns in binary-matrix
+    """
+    result = {}
+    for column in data.columns:
+        tmp = data.loc[data[column] == 1].copy()
+        tmp = tmp.sum()
+        tmp /= tmp[column]
+        tmp = tmp.drop(column)
+        tmp = tmp.loc[tmp.values >= dependency_level]
+        new_row = {}
+        if tmp.size > 0:
+            for index in tmp.index:
+                new_row[index] = tmp[index]
+            result[column] = new_row
+    return pd.DataFrame(result).fillna(0)
