f337b0a701fc81f953ba1072076f40bc3bf084bd
Dan <dan.chiriac1453@gmail.com>
2021-04-24 15:34:34 +0300

    Tested the presence of new added columns

--- tests/unit/test_pwdb_transformer.py
+++ tests/unit/test_pwdb_transformer.py
@@ -10,8 +10,10 @@ def test_pwdb_transformer(raw_pwdb_data):
     assert len(raw_pwdb_data) == 2
     transformed_data_sample = transform_pwdb(raw_pwdb_data)[0]
 
+    assert type(transformed_data_sample) == dict
     assert "Title" in transformed_data_sample
     assert "Country" in transformed_data_sample
+    assert "Occupations" in transformed_data_sample
+    assert "Sectors" in transformed_data_sample
     assert "03/27/2020" == transformed_data_sample["Start date"]
     assert "applications for phase" in transformed_data_sample["Use of measure"]
-
