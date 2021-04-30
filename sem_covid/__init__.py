3617d0b3cc8b994666d50523f445f390f3bad5e8
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2021-04-30 12:08:01 +0300

    add unit tests

--- sem_covid/__init__.py
+++ sem_covid/__init__.py
@@ -10,7 +10,7 @@
 """
 import logging
 import warnings
-
+import pathlib
 import dotenv
 
 from sem_covid.base_config import BaseConfig
@@ -198,8 +198,7 @@ class SemCovidConfig(object):
     def ELASTICSEARCH_PASSWORD(self) -> str:
         return BaseConfig.find_value()
 
-<<<<<<< Updated upstream
-=======
+
     #Crawler property
     @property
     def CRAWLER_EU_TIMELINE_SPOKEPERSONS(self) -> str:
@@ -209,7 +208,7 @@ class SemCovidConfig(object):
     def CRAWLER_EU_TIMELINE_PRESS_ASSISTANT(self) -> str:
         return str(pathlib.Path(__file__).resolve().parents[1] / "resources" / "crawlers" / "eu_timeline_press_assistant_28_04_2021.json")
 
->>>>>>> Stashed changes
+
 
 
 
