6139a2602b75197291e69b2f6b67134365c4b309
Dan <dan.chiriac1453@gmail.com>
2021-05-06 11:27:15 +0300

    removed unused part of function

--- tests/features/conftest.py
+++ tests/features/conftest.py
@@ -35,24 +35,4 @@ def elasticsearch_client():
                                    config.ELASTICSEARCH_HOST_NAME + ':' +
                                    str(config.ELASTICSEARCH_PORT)])
 
-    # with open(config.ES_PWDB_INDEX_MAPPING_FILE) as json_file:
-    #     mapping = json.load(json_file)
-    # print("Creating index " + config.PWDB_ELASTIC_SEARCH_INDEX_NAME)
-    # elasticsearch.indices.create(index=config.PWDB_ELASTIC_SEARCH_INDEX_NAME, body=mapping, ignore=400)
-    #
-    # path_list = Path(config.PWDB_ES_TEST_DATA_DIRECTORY) / Path('tika')
-    # count = 0
-    # for path in path_list.glob('*.*'):
-    #     with open(Path(path)) as json_file:
-    #         payload = json.load(json_file)
-    #     res = elasticsearch.index(index=config.PWDB_ELASTIC_SEARCH_INDEX_NAME, body=payload)
-    #     count += 1
-    #     print(str(count) + " - sent  " + str(path) + " - the result was " + str(res))
-    # print("Forcing index refresh...")
-    #
-    # elasticsearch.indices.refresh(index=config.PWDB_ELASTIC_SEARCH_INDEX_NAME)
-    # yield elasticsearch
-    #
-    # print("Deleting index " + config.PWDB_ELASTIC_SEARCH_INDEX_NAME)
-    # elasticsearch.indices.delete(index=config.PWDB_ELASTIC_SEARCH_INDEX_NAME)
     return elasticsearch
