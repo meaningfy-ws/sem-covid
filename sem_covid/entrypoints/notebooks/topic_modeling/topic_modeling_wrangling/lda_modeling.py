e88db081c11e2012661319316ea3c120f50a873f
DanCh11 <48621937+DanCh11@users.noreply.github.com>
2021-05-26 11:37:35 +0300

    Removed unused function and changed the class arguments

--- sem_covid/entrypoints/notebooks/topic_modeling/topic_modeling_wrangling/lda_modeling.py
+++ sem_covid/entrypoints/notebooks/topic_modeling/topic_modeling_wrangling/lda_modeling.py
@@ -1,11 +1,14 @@
 
+from typing import List
+
 import pandas as pd
-from gensim.models import LdaMulticore, TfidfModel
+from gensim.models import LdaMulticore
 import gensim.corpora as corpora
 
 
 class WordsModeling(object):
-    def __init__(self, document: pd.Series):
+
+    def __init__(self, document: List):
         self.document = document
         self.id2word = corpora.Dictionary(self.document)
         self.corpus = [self.id2word.doc2bow(text) for text in self.document]
@@ -16,9 +19,3 @@ class WordsModeling(object):
         """
         return LdaMulticore(corpus=self.corpus, id2word=self.id2word, num_topics=10, random_state=100, chunksize=10,
                             passes=10, alpha="symmetric", iterations=100, per_word_topics=True)
-
-    def words_frequency(self) -> TfidfModel:
-        """
-        calculates TF-IDF for created corpus
-        """
-        return TfidfModel(self.corpus)
