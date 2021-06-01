98066fc38ad1a7def4f6d92594bbe9e545945788
costezki <costezki.eugen@gmail.com>
2021-06-01 13:39:31 +0200

    regex shall use regex patterns not strings

--- sem_covid/services/sc_wrangling/data_cleaning.py
+++ sem_covid/services/sc_wrangling/data_cleaning.py
@@ -86,16 +86,16 @@ def prepare_text_for_cleaning(text: str):
     # remove links
     text = re.sub(r"^https?:\/\/.*[\r\n]*", '', text)
     # remove "new line" symbol
-    text = re.sub('\n', '', text)
+    text = re.sub(r'\n', '', text)
     # Match every decimal digits and every character marked as letters in Unicode database
-    text = re.sub('\w*\d\w*', '', text)
+    text = re.sub(r'\w*\d\w*', '', text)
     # Delete square brackets
-    text = re.sub('\[.*?\]', '', text)
-    text = re.sub('[‘’“”…]', '', text)
+    text = re.sub(r'\[.*?\]', '', text)
+    text = re.sub(r'[‘’“”…]', '', text)
     # remove punctuation
-    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
     text = ''.join(text)
-    text = re.split('\W+', text)
+    text = re.split(r'\W+', text)
     text = [word for word in text if word not in stopword]
 
     return text
