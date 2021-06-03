8a5324327f0153193ca3f5d56e44dd245866f2dc
costezki <costezki.eugen@gmail.com>
2021-06-03 11:30:13 +0200

    DAG testing works now

--- tests/dags/__init__.py
+++ tests/dags/__init__.py
@@ -6,8 +6,3 @@
 # Email: costezki.eugen@gmail.com 
 
 """ """
-import os
-from pathlib import Path
-
-TEST_AIRFLOW_DAG_FOLDER = Path(__file__).parent.parent.parent / "sem_covid/"
-os.environ["AIRFLOW_HOME"] = str(TEST_AIRFLOW_DAG_FOLDER)
