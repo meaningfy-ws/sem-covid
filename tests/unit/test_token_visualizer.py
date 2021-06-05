9557ef518bdaab130aa60e67ed53a4061a9c463e
DanCh11 <48621937+DanCh11@users.noreply.github.com>
2021-06-05 18:02:31 +0300

    unfinished topic modeling tests

---
+++ tests/unit/test_token_visualizer.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+from sem_covid.entrypoints.notebooks.topic_modeling.topic_modeling_wrangling.token_management import *
+
+
+def test_filter_stop_words(tokenized_textual_pwdb_series):
+    corpus_without_stopwords = tokenized_textual_pwdb_series.apply(filter_stop_words, spacy_stop_words)
+
+    assert pd.Series == type(corpus_without_stopwords)
+    assert Token == type(corpus_without_stopwords[0])
+
+
+def test_filter_pos(tokenized_textual_pwdb_series):
+    document = tokenized_textual_pwdb_series.apply(filter_pos, pos="PUNCT")
+
+    assert pd.Series == type(document)
+
+
+def test_select_pos(tokenized_textual_pwdb_series):
+    pass
+
+
+def test_filter_stop_words_on_a_span_list(tokenized_textual_pwdb_series):
+    pass
