5bb7e93aea0f3d1e2bc6342e49bec71b52d83f48
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2021-06-10 17:09:30 +0300

    Create model_registry.py

---
+++ sem_covid/services/model_registry.py
@@ -0,0 +1,29 @@
+class ClassificationModels:
+
+    @property
+    def PWDB_BUSINESSES(self):
+        pass
+
+    @property
+    def PWDB_CITIZENS(self):
+        pass
+
+    @property
+    def PWDB_WORKERS(self):
+        pass
+
+    @property
+    def PWDB_CATEGORY(self):
+        pass
+
+    @property
+    def PWDB_SUBCATEGORY(self):
+        pass
+
+    @property
+    def PWDB_TYPE_OF_MEASURE(self):
+        pass
+
+    @property
+    def PWDB_TARGET_GROUPS(self):
+        pass
