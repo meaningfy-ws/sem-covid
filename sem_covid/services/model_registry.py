61c32f699b3129730ca6324c378940a5e1b61768
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2021-06-10 19:30:08 +0300

    add model registry

--- sem_covid/services/model_registry.py
+++ sem_covid/services/model_registry.py
@@ -1,29 +1,54 @@
-class ClassificationModels:
+import mlflow
+
+
+def get_best_model_from_ml_flow(experiment_ids: list, metric_name: str = 'F1'):
+    runs_df = mlflow.search_runs(experiment_ids=experiment_ids, filter_string="")
+    id_run = runs_df.loc[runs_df[f"metrics.{metric_name}"].idxmax()]['run_id']
+    return mlflow.sklearn.load_model("runs:/" + id_run + "/model")
+
+
+class ClassificationModel:
+
+    @staticmethod
+    def pwdb_by_class_name(class_name: str):
+        class_name_to_exp_id = {
+            'businesses': ['12'],
+            'citizens': ['13'],
+            'workers': ['14'],
+            "category": ['15'],
+            "subcategory": ['20'],
+            "type_of_measure": ['21'],
+            "target_groups": ['22']
+        }
+        if class_name in class_name_to_exp_id.keys():
+            return get_best_model_from_ml_flow(experiment_ids=class_name_to_exp_id[class_name])
+        else:
+            return None
 
     @property
     def PWDB_BUSINESSES(self):
-        pass
+        return get_best_model_from_ml_flow(experiment_ids=['12'])
 
     @property
     def PWDB_CITIZENS(self):
-        pass
+        return get_best_model_from_ml_flow(experiment_ids=['13'])
 
     @property
     def PWDB_WORKERS(self):
-        pass
+        return get_best_model_from_ml_flow(experiment_ids=['14'])
 
     @property
     def PWDB_CATEGORY(self):
-        pass
+        return get_best_model_from_ml_flow(experiment_ids=['15'])
 
     @property
     def PWDB_SUBCATEGORY(self):
-        pass
+        return get_best_model_from_ml_flow(experiment_ids=['20'])
 
     @property
     def PWDB_TYPE_OF_MEASURE(self):
-        pass
+        return get_best_model_from_ml_flow(experiment_ids=['21'])
 
     @property
     def PWDB_TARGET_GROUPS(self):
-        pass
+        return get_best_model_from_ml_flow(experiment_ids=['22'])
---
+++ tests/e2e/_test_model_registry.py
@@ -0,0 +1,11 @@
+from sem_covid.services.model_registry import ClassificationModel
+
+
+def test_model_registry():
+    assert ClassificationModel.PWDB_BUSINESSES is not None
+    assert ClassificationModel.PWDB_WORKERS is not None
+    assert ClassificationModel.PWDB_CATEGORY is not None
+    assert ClassificationModel.PWDB_CITIZENS is not None
+    assert ClassificationModel.PWDB_SUBCATEGORY is not None
+    assert ClassificationModel.PWDB_TARGET_GROUPS is not None
+    assert ClassificationModel.PWDB_TYPE_OF_MEASURE is not None
