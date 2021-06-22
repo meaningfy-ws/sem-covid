3530b1a05feaab86b8198b9f3b382be020579c02
DanCh11 <dan.chiriac1453@gmail.com>
2021-06-22 14:32:23 +0200

    Added roc_auc_score metric

--- sem_covid/services/sc_wrangling/evaluation_metrics.py
+++ sem_covid/services/sc_wrangling/evaluation_metrics.py
@@ -2,7 +2,7 @@ import pandas as pd
 from numpy.core.records import ndarray
 from pandas import Series
 from sklearn.metrics import (f1_score, precision_score, accuracy_score,
-                             recall_score, mean_squared_error, mean_absolute_error)
+                             recall_score, mean_squared_error, mean_absolute_error, roc_auc_score)
 
 
 def model_evaluation_metrics(actual_data: Series, predicted_data: ndarray):
@@ -19,11 +19,12 @@ def model_evaluation_metrics(actual_data: Series, predicted_data: ndarray):
     f1 = f1_score(actual_data, predicted_data, average="macro")
     mae = mean_absolute_error(actual_data, predicted_data)
     mse = mean_squared_error(actual_data, predicted_data)
+    roc_auc = roc_auc_score(actual_data, predicted_data, multi_class='ovr')
 
     metric_lables = ['Accuracy', 'Precision', 'Recall',
                      'F1-Score', 'Mean Absolute Error',
-                     'Mean Squared Error']
+                     'Mean Squared Error', 'ROC AUC Score']
 
-    metric_values = [accuracy, precision, recall, f1, mae, mse]
+    metric_values = [accuracy, precision, recall, f1, mae, mse, roc_auc]
 
     return {key: values for key, values in zip(metric_lables, metric_values)}
