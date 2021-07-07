7e05015ee3d92ac5aa0d1b755386504b6f10e930
DanCh11 <dan.chiriac1453@gmail.com>
2021-07-07 15:49:52 +0200

    replaced our get_similarity_matrix function with sklearn function.

--- sem_covid/entrypoints/notebooks/language_modeling/language_model_tools/similarity_calculus.py
+++ sem_covid/entrypoints/notebooks/language_modeling/language_model_tools/similarity_calculus.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 from gensim.models import KeyedVectors
+from sklearn.metrics import pairwise_distances
 
 
 def euclidean_similarity(vector_1: np.array, vector_2: np.array) -> np.float:
@@ -25,17 +26,30 @@ def manhattan_similarity(vector_1: np.array, vector_2: np.array) -> np.float:
     return sum(abs(value_1 - value_2) for value_1, value_2 in zip(vector_1, vector_2))
 
 
-def get_similarity_matrix(wv: KeyedVectors, similarity_function) -> pd.DataFrame:
+# def get_similarity_matrix(wv: KeyedVectors, similarity_function) -> pd.DataFrame:
+#     """
+#         creates a dataframe based on keys and vectors from pretrained gensim model
+#         and selected similarity function
+#
+#         :param wv: the object that contains the mapping between words and embeddings
+#         :param similarity_function: function that helps to measure the distance between word vectors
+#         :return: dataframe with similarity of each word
+#     """
+#     similarity_matrix_columns = wv.index_to_key
+#     return pd.DataFrame([{column_index: similarity_function(wv[row_index], wv[column_index])
+#                           for column_index in similarity_matrix_columns}
+#                         for row_index in similarity_matrix_columns],
+#                         columns=similarity_matrix_columns, index=[similarity_matrix_columns])
+
+
+def get_similarity_matrix(vector: np.ndarray, keys: list, metric=callable) -> pd.DataFrame:
     """
         creates a dataframe based on keys and vectors from pretrained gensim model
         and selected similarity function
 
-        :param wv: the object that contains the mapping between words and embeddings
-        :param similarity_function: function that helps to measure the distance between word vectors
+        :param vector: the object that contains the mapping between words and embeddings
+        :param keys: words indexes keys
+        :metric: metric distance formula
         :return: dataframe with similarity of each word
     """
-    similarity_matrix_columns = wv.index_to_key
-    return pd.DataFrame([{column_index: similarity_function(wv[row_index], wv[column_index])
-                          for column_index in similarity_matrix_columns}
-                        for row_index in similarity_matrix_columns],
-                        columns=similarity_matrix_columns, index=[similarity_matrix_columns])
+    return pd.DataFrame(pairwise_distances(vector, metric=metric), columns=keys, index=keys)
