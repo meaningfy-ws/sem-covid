1ed767591d9deb31663d2ba269ecbe376ca2d874
DanCh11 <dan.chiriac1453@gmail.com>
2021-07-27 20:52:46 +0200

    adapted crawler pipeline changes to the dags

--- sem_covid/entrypoints/etl_dags/crawl_eu_action_timeline.py
+++ sem_covid/entrypoints/etl_dags/crawl_eu_action_timeline.py
@@ -7,6 +7,7 @@ from airflow.operators.python import PythonOperator
 from sem_covid import config
 from sem_covid.services.crawlers.scrapy_crawlers.spiders.eu_timeline_spider import EUTimelineSpider
 from sem_covid.entrypoints.etl_dags.crawl_pipeline import CrawlDagPipeline
+from sem_covid.services.store_registry import store_registry
 
 VERSION = '0.2.5'
 DATASET_NAME = "eu_timeline"
@@ -15,9 +16,13 @@ DAG_NAME = DAG_TYPE + '_' + DATASET_NAME + '_' + VERSION
 TIKA_FILE_PREFIX = 'tika/'
 CONTENT_PATH_KEY = 'detail_content'
 
-crawl_dag_pipeline = CrawlDagPipeline(config.EU_TIMELINE_JSON, config.EU_TIMELINE_BUCKET_NAME,
-                                      config.EU_TIMELINE_ELASTIC_SEARCH_INDEX_NAME, CONTENT_PATH_KEY,
-                                      EUTimelineSpider)
+crawl_dag_pipeline = CrawlDagPipeline(
+    store_registry=store_registry,
+    file_name=config.EU_TIMELINE_JSON,
+    bucket_name=config.EU_TIMELINE_BUCKET_NAME,
+    elasticsearch_index_name=config.EU_TIMELINE_ELASTIC_SEARCH_INDEX_NAME,
+    content_path_key=CONTENT_PATH_KEY,
+    scrapy_crawler=EUTimelineSpider)
 
 default_args = {
     "owner": "airflow",
@@ -40,14 +45,14 @@ dag = DAG(
 
 start_crawler = PythonOperator(
     task_id=f'Crawl',
-    python_callable=crawl_dag_pipeline.start_crawler, retries=1, dag=dag)
+    python_callable=crawl_dag_pipeline.extract, retries=1, dag=dag)
 
 extract_content_with_tika_task = PythonOperator(
     task_id=f'Tika',
-    python_callable=crawl_dag_pipeline.extract_document_content_with_tika, retries=1, dag=dag)
+    python_callable=crawl_dag_pipeline.transform_content, retries=1, dag=dag)
 
 upload_to_elastic_task = PythonOperator(
     task_id=f'Elasticsearch',
-    python_callable=crawl_dag_pipeline.upload_processed_documents_to_elasticsearch, retries=1, dag=dag)
+    python_callable=crawl_dag_pipeline.load, retries=1, dag=dag)
 
 start_crawler >> extract_content_with_tika_task >> upload_to_elastic_task
--- sem_covid/entrypoints/etl_dags/crawl_irish_action_timeline.py
+++ sem_covid/entrypoints/etl_dags/crawl_irish_action_timeline.py
@@ -15,6 +15,7 @@ from airflow.operators.python import PythonOperator
 from sem_covid import config
 from sem_covid.services.crawlers.scrapy_crawlers.spiders.irish_gov import IrishGovCrawler
 from sem_covid.entrypoints.etl_dags.crawl_pipeline import CrawlDagPipeline
+from sem_covid.services.store_registry import store_registry
 
 VERSION = '0.1.4'
 DATASET_NAME = "ireland_timeline"
@@ -23,9 +24,13 @@ DAG_NAME = DAG_TYPE + '_' + DATASET_NAME + '_' + VERSION
 TIKA_FILE_PREFIX = 'tika/'
 CONTENT_PATH_KEY = 'content'
 
-crawl_dag_pipeline = CrawlDagPipeline(config.IRELAND_TIMELINE_JSON, config.IRELAND_TIMELINE_BUCKET_NAME,
-                                      config.IRELAND_TIMELINE_ELASTIC_SEARCH_INDEX_NAME, CONTENT_PATH_KEY,
-                                      IrishGovCrawler)
+crawl_dag_pipeline = CrawlDagPipeline(
+    store_registry=store_registry,
+    file_name=config.IRELAND_TIMELINE_JSON,
+    bucket_name=config.IRELAND_TIMELINE_BUCKET_NAME,
+    elasticsearch_index_name=config.IRELAND_TIMELINE_ELASTIC_SEARCH_INDEX_NAME,
+    content_path_key=CONTENT_PATH_KEY,
+    scrapy_crawler=IrishGovCrawler)
 
 default_args = {
     "owner": "airflow",
@@ -48,14 +53,14 @@ dag = DAG(
 
 start_crawler = PythonOperator(
     task_id=f'Crawl',
-    python_callable=crawl_dag_pipeline.start_crawler, retries=1, dag=dag)
+    python_callable=crawl_dag_pipeline.extract, retries=1, dag=dag)
 
 extract_content_with_tika_task = PythonOperator(
     task_id=f'Tika',
-    python_callable=crawl_dag_pipeline.extract_document_content_with_tika, retries=1, dag=dag)
+    python_callable=crawl_dag_pipeline.transform_content, retries=1, dag=dag)
 
 upload_to_elastic_task = PythonOperator(
     task_id=f'Elasticsearch',
-    python_callable=crawl_dag_pipeline.upload_processed_documents_to_elasticsearch, retries=1, dag=dag)
+    python_callable=crawl_dag_pipeline.load, retries=1, dag=dag)
 
 start_crawler >> extract_content_with_tika_task >> upload_to_elastic_task
