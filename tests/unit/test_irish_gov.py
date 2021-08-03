e56fbdb00ec6456b62649b5612d3d047fc8b30d5
DanCh11 <dan.chiriac1453@gmail.com>
2021-08-03 14:20:26 +0200

    checkpoint

--- tests/unit/test_irish_gov.py
+++ tests/unit/test_irish_gov.py
@@ -2,6 +2,7 @@ import json
 import os
 import pathlib
 
+import numpy as np
 import requests
 from scrapy.http import Request, HtmlResponse
 
@@ -11,14 +12,15 @@ from tests.unit.test_store.fake_store_registry import FakeStoreRegistry
 
 
 MOCK_ARTICLE_PAGE = '/home/daycu/Programming/Meaningfy/sem-covid/tests/test_data/crawlers/sample_ireland_gov/innovation_keyword.html'
-MOCK_MILK_SEARCH_PAGE = '/home/daycu/Programming/Meaningfy/sem-covid/tests/test_data/crawlers/sample_ireland_gov/milk_keyword.html'
-MOCK_INNOVATION_SEARCH_PAGE = '/home/daycu/Programming/Meaningfy/sem-covid/tests/test_data/crawlers/sample_ireland_gov/mock_covid_search_terms_pages/innovation.html'
+MOCK_PUBLICATION = '/home/daycu/Programming/Meaningfy/sem-covid/tests/test_data/crawlers/sample_ireland_gov/mock_publication.html'
+
+mock_article_page_file = pathlib.Path(__file__).parent.parent.parent / 'tests' / 'test_data' / 'crawlers' / 'sample_ireland_gov' / 'saved_data' / 'mock_article_page.json'
+mock_publication_file = pathlib.Path(__file__).parent.parent.parent / 'tests' / 'test_data' / 'crawlers' / 'sample_ireland_gov' / 'saved_data' / 'mock_publication.json'
+mock_covid_search_term_page_file = pathlib.Path(__file__).parent.parent.parent / 'tests' / 'test_data' / 'crawlers' / 'sample_ireland_gov' / 'saved_data' / 'mock_covid_search_term_page.json'
+
 FAKE_FILENAME = 'fake_filename.json'
 FAKE_BUCKET_NAME = 'fake-bucket-name'
-KEY_WORD = ['milk']
-mock_article_page_file = pathlib.Path(__file__).parent.parent.parent / 'tests' / 'test_data' / 'crawlers' / 'sample_ireland_gov' / 'saved_data' / 'mock_article_page.json'
-mock_milk_search_page_file = pathlib.Path(__file__).parent.parent.parent / 'tests' / 'test_data' / 'crawlers' / 'sample_ireland_gov' / 'saved_data' / 'mock_milk_search_page.json'
-mock_covid_search_term_page_file = pathlib.Path(__file__).parent.parent.parent / 'tests' / 'test_data' / 'crawlers' / 'sample_ireland_gov' / 'saved_data' / 'mock_covid_search_term_page.json'
+KEY_WORD = ['innovation']
 store_registry = FakeStoreRegistry()
 
 
@@ -31,7 +33,7 @@ def fake_response_from_file(file_name: str, url: str = None) -> HtmlResponse:
     returns: A scrapy HTTP response which can be used for unit testing.
     """
     if not url:
-        url = 'https://www.gov.ie/en/publications/?q=milk&sort_by=published_date'
+        url = 'https://www.gov.ie/en/publications/?q=innovation'
     if not file_name[0] == '/':
         responses_dir = os.path.dirname(os.path.realpath(__file__))
         file_path = os.path.join(responses_dir, file_name)
@@ -57,29 +59,29 @@ def create_html_response(url: str) -> HtmlResponse:
     return HtmlResponse(url=url, request=Request(url=url), body=extract_html_content_text(url), encoding='utf-8')
 
 
-def test_irish_gov_crawler_parsing_detail_page_method():
+def test_irish_gov_crawler_parsing_publication_from_the_page():
     irish_gov_crawler = IrishGovCrawler(
         text_searches=COVID_EUROVOC_SEARCH_TERMS[0],
         filename=FAKE_FILENAME,
         storage_adapter=store_registry.minio_object_store(FAKE_BUCKET_NAME),
         )
-    result = irish_gov_crawler.parse_detail_page(fake_response_from_file(MOCK_ARTICLE_PAGE))
+    result = irish_gov_crawler.parse_detail_page(fake_response_from_file(MOCK_PUBLICATION))
     result = irish_gov_crawler.data
-    mock_article_page_file.write_text(data=json.dumps(result))
+    mock_publication_file.write_text(data=json.dumps(result))
 
 
-def test_irish_gov_crawler_search_milk():
+def test_irish_gov_crawler_innovation_keyword():
     irish_gov_crawler = IrishGovCrawler(
         text_searches=KEY_WORD,
         filename=FAKE_FILENAME,
         storage_adapter=store_registry.minio_object_store(FAKE_BUCKET_NAME),
         )
-    parsed_pages = irish_gov_crawler.parse(fake_response_from_file(MOCK_MILK_SEARCH_PAGE))
+    parsed_pages = irish_gov_crawler.parse(fake_response_from_file(MOCK_ARTICLE_PAGE))
     for page in list(parsed_pages):
         irish_gov_crawler.parse_detail_page(create_html_response(page.url))
 
     output = irish_gov_crawler.data
-    mock_milk_search_page_file.write_text(data=json.dumps(output))
+    mock_article_page_file.write_text(data=json.dumps(output))
 
 
 def test_irish_gov_crawler_for_key_word_innovation():
@@ -87,22 +89,13 @@ def test_irish_gov_crawler_for_key_word_innovation():
         text_searches=COVID_EUROVOC_SEARCH_TERMS,
         filename=FAKE_FILENAME,
         storage_adapter=store_registry.minio_object_store(FAKE_BUCKET_NAME))
-
-    parsed_pages = irish_gov_crawler.parse(create_html_response('https://www.gov.ie/en/publications/?q=innovation'))
-    for each_page in list(parsed_pages):
-        irish_gov_crawler.parse_detail_page(create_html_response(each_page.url))
-    output = irish_gov_crawler.data
-    mock_covid_search_term_page_file.write_text(data=json.dumps(output))
-
-
-def test_irish_gov_crawler_innovation_multiple_pages():
-    irish_gov_crawler = IrishGovCrawler(
-        text_searches=COVID_EUROVOC_SEARCH_TERMS,
-        filename=FAKE_FILENAME,
-        storage_adapter=store_registry.minio_object_store(FAKE_BUCKET_NAME))
-
-    parsed_pages = irish_gov_crawler.parse(create_html_response('https://www.gov.ie/en/publications/?q=innovation'))
-
+    page_numbers = np.arange(1, 9, 1)
+    for page_number in page_numbers:
+        url = 'https://www.gov.ie/en/publications/?q=innovation&page='
+        numbered_page = url + str(page_number)
+        parsed_pages = irish_gov_crawler.parse(create_html_response(numbered_page))
+        for each_page in list(parsed_pages):
+            irish_gov_crawler.parse_detail_page(create_html_response(each_page.url))
     output = irish_gov_crawler.data
     print(output)
-
+    mock_covid_search_term_page_file.write_text(data=json.dumps(output))
