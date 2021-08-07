eccead3f812b16626ee3ff65ff9bccbef61ff96c
DanCh11 <dan.chiriac1453@gmail.com>
2021-08-07 19:37:47 +0200

    function that converts jupyter notebooks into html

---
+++ sem_covid/services/sc_wrangling/notebook_publication.py
@@ -0,0 +1,9 @@
+import os
+import pathlib
+
+
+def convert_notebook_to_html(notebook_path: pathlib.Path, published_html_path: pathlib.Path) -> os.system:
+    """
+    This function allows us to publish jupyter notebooks in html extension
+    """
+    return os.system(f"jupyter nbconvert --output-dir={published_html_path} --to html {notebook_path}")
