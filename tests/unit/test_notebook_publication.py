0ef300bce14fc96bb985499492a33d4817393fb8
DanCh11 <dan.chiriac1453@gmail.com>
2021-08-07 19:38:27 +0200

    test for notebook publication function

---
+++ tests/unit/test_notebook_publication.py
@@ -0,0 +1,9 @@
+
+import os
+from tests.unit.conftest import call_test_notebook, output_dir_for_converted_notebook
+from sem_covid.services.sc_wrangling.notebook_publication import convert_notebook_to_html
+
+
+def test_convert_notebook_to_html():
+    convert = convert_notebook_to_html(call_test_notebook(), output_dir_for_converted_notebook())
+    assert int == type(convert)
