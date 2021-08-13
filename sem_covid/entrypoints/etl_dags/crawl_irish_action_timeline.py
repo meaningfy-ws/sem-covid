dda44c2b37cc01e38dbf3f1e37324517cf738457
DanCh11 <dan.chiriac1453@gmail.com>
2021-08-13 12:22:43 +0200

    added airflow module and logger

--- sem_covid/entrypoints/etl_dags/crawl_irish_action_timeline.py
+++ sem_covid/entrypoints/etl_dags/crawl_irish_action_timeline.py
@@ -3,15 +3,22 @@
 # crawl_irish_action_timeline.py
 # Date:  13/04/2021
 # Author: Eugeniu Costetchi
-# Email: costezki.eugen@gmail.com 
+# Email: costezki.eugen@gmail.com
+import logging
 
+import airflow
 
 from sem_covid import config
 from sem_covid.adapters.dag.dag_factory import DagFactory
 from sem_covid.entrypoints import dag_name
-from sem_covid.services.crawlers.scrapy_crawlers.spiders.irish_gov import IrishGovCrawler
 from sem_covid.entrypoints.etl_dags.crawl_pipeline import CrawlDagPipeline
+from sem_covid.services.crawlers.scrapy_crawlers.spiders.irish_gov import IrishGovCrawler
 from sem_covid.services.store_registry import store_registry
+
+logger = logging.getLogger(__name__)
+logger.debug(f"This line is important for DAG discovery because the *airflow module* "
+             f"shall be imported here. Otherwise it does not discover DAGs in this "
+             f"module. Airflow version {airflow.__version__}")
 
 CONTENT_PATH_KEY = 'content'
 DAG_TYPE = 'etl'
