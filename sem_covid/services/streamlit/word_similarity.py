9d6992550369a10c3e7c0dd583c98031b6803a28
DanCh11 <dan.chiriac1453@gmail.com>
2021-08-20 16:34:56 +0300

    got visualization of the graphs in streamlit

--- sem_covid/services/streamlit/word_similarity.py
+++ sem_covid/services/streamlit/word_similarity.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-from streamlit.components import v1 as components
+import streamlit.components.v1 as components
 import pandas as pd
 import networkx as nx
 import numpy as np
@@ -56,7 +56,7 @@ def generate_graph(similarity_matrix: pd.DataFrame, graph: nx.Graph, root_word: 
     return graph
 
 
-@st.cache
+@st.cache(suppress_st_warning=True)
 def create_similarity_graph(similarity_matrix: pd.DataFrame, key_word: str, metric_threshold: np.float64,
                             top_words: int) -> d3graph:
     color_map = {0: '#a70000',
@@ -70,30 +70,22 @@ def create_similarity_graph(similarity_matrix: pd.DataFrame, key_word: str, metr
     network_adjacency_matrix = pd.DataFrame(data=nx.adjacency_matrix(graph).todense(),
                                             index=graph.nodes(), columns=graph.nodes())
     node_color_list = [deep_map[node][0] for node in graph.nodes()]
-    return d3graph(network_adjacency_matrix, node_color=node_color_list, width=1920, height=1080, edge_width=5,
-                   edge_distance=60, directed=True)
+    return d3graph(network_adjacency_matrix, node_color=node_color_list, width=650, height=500, edge_width=5,
+                   edge_distance=60, directed=True, showfig=False)
 
 
 st.title('Semantic similarity graph')
 
 word = st.text_input("Introduce word")
-
 threshold_slider = st.slider('Threshold', min_value=0.0, max_value=1.0, step=0.1, value=0.4)
 number_of_neighbours_slider = st.slider('Number of Neighbours', min_value=1, max_value=5, step=1, value=1)
 # graph_depth_slider = st.slider('Graph Depth', min_value=2, max_value=5, step=1, value=0)
 
 
-st.write(word)
-
-sliders = [threshold_slider, number_of_neighbours_slider]
-
-for slider in sliders:
-    st.write(slider)
-
-
-if st.button('generate'):
-    create_similarity_graph(similarity_matrix=read_similarity_matrix(BUCKET_NAME, MODEL_NAME),
-                            key_word=word, top_words=number_of_neighbours_slider,
-                            metric_threshold=threshold_slider)
-
-
+if st.button('Generate'):
+    graph = create_similarity_graph(similarity_matrix=read_similarity_matrix(BUCKET_NAME, MODEL_NAME),
+                                    key_word=word, top_words=number_of_neighbours_slider,
+                                    metric_threshold=threshold_slider)
+    html_file = open(graph['path'], 'r', encoding='utf-8')
+    source_code = html_file.read()
+    components.html(source_code, width=700, height=700)
