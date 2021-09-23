fc2c78ee8d4223277c2725ffaafa7fb3a1a0824f
DanCh11 <dan.chiriac1453@gmail.com>
2021-09-23 17:45:54 +0300

    made tests pass

--- tests/unit/dags/test_crawl_eu_action_timeline_dag.py
+++ tests/unit/dags/test_crawl_eu_action_timeline_dag.py
@@ -13,22 +13,22 @@ def test_crawl_eu_action_timeline_has_three_tasks_and_order(airflow_dag_bag):
     assert dag is not None
     tasks = dag.tasks
     task_ids = list(map(lambda task: task.task_id, tasks))
-    assert {'extract', 'transform_content', 'load'}.issubset(set(task_ids))
+    assert {'extract', 'transform_structure', 'transform_content', 'load'}.issubset(set(task_ids))
 
     crawl_task = dag.get_task('extract')
     upstream_task_id = list(map(lambda task: task.task_id, crawl_task.upstream_list))
     assert not upstream_task_id
     downstream_task_id = list(map(lambda task: task.task_id, crawl_task.downstream_list))
+    assert 'transform_structure' in downstream_task_id
+
+    tika_task = dag.get_task('transform_structure')
+    upstream_task_id = list(map(lambda task: task.task_id, tika_task.upstream_list))
+    assert 'extract' in upstream_task_id
+    downstream_task_id = list(map(lambda task: task.task_id, tika_task.downstream_list))
     assert 'transform_content' in downstream_task_id
 
     tika_task = dag.get_task('transform_content')
     upstream_task_id = list(map(lambda task: task.task_id, tika_task.upstream_list))
-    assert 'extract' in upstream_task_id
+    assert 'transform_structure' in upstream_task_id
     downstream_task_id = list(map(lambda task: task.task_id, tika_task.downstream_list))
     assert 'load' in downstream_task_id
-
-    elastic_search_task = dag.get_task('load')
-    upstream_task_id = list(map(lambda task: task.task_id, elastic_search_task.upstream_list))
-    assert 'transform_content' in upstream_task_id
-    downstream_task_id = list(map(lambda task: task.task_id, elastic_search_task.downstream_list))
-    assert not downstream_task_id
