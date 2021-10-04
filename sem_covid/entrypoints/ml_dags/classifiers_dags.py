f3592d7c570983f084efcff5af34d16b9d8608ba
DanCh11 <48621937+DanCh11@users.noreply.github.com>
2021-10-04 14:50:05 +0300

    added tf_hug to the requirements

--- sem_covid/entrypoints/ml_dags/classifiers_dags.py
+++ sem_covid/entrypoints/ml_dags/classifiers_dags.py
@@ -26,7 +26,7 @@ CATEGORY = "ml"
 EXPERIMENT_NAME = "PyCaret_pwdb"
 PWDB_FEATURE_STORE_NAME = 'fs_pwdb'
 
-REQUIREMENTS = ['pycaret', 'mlflow']
+REQUIREMENTS = ['pycaret', 'mlflow', 'tensorflow_hub']
 
 # Word-Embedding-AVG classifiers
 
