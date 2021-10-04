bdffc532cd1b396a074e370941a76909d9ee8f85
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2021-10-04 09:13:02 +0300

    Update semantic_similarity.py

--- sem_covid/entrypoints/streamlit_visualizers/semantic_similarity.py
+++ sem_covid/entrypoints/streamlit_visualizers/semantic_similarity.py
@@ -20,28 +20,19 @@ from sem_covid.services.model_registry import embedding_registry
 from sem_covid.services.store_registry import store_registry
 import plotly.express as px
 
-UNIFIED_DATASET = 'ds_unified_datasets'
-
 
 def load_data_in_cache():
     if not hasattr(st, 'easy_cache'):
         es_store = store_registry.es_index_store()
         st.easy_cache = dict()
-        st.easy_cache['pwdb_df'] = es_store.get_dataframe(index_name=config.PWDB_ELASTIC_SEARCH_INDEX_NAME)
-        unified_df = es_store.get_dataframe(index_name=UNIFIED_DATASET)
-        emb_model = embedding_registry.sent2vec_universal_sent_encoding()
-        unified_df = pd.DataFrame(unified_df[unified_df.Document_source == 'pwdb'])
-        unified_df['text'] = unified_df[['Title', 'Content']].agg(' '.join, axis=1)
-        unified_df['emb'] = emb_model.encode(unified_df['text'].values)
-        st.easy_cache['unified_df'] = unified_df
+        st.easy_cache['unified_df'] = #es_store.get_dataframe(index_name=config.UNIFIED_DATASET_ELASTIC_SEARCH_INDEX_NAME)
         st.write('Data cached!')
     return st.easy_cache
 
 
 app_cache = load_data_in_cache()
-pwdb_df = app_cache['pwdb_df']
 unified_df = app_cache['unified_df']
-countries = list(unique_everseen(pwdb_df.country.values))
+countries = list(unique_everseen(unified_df.country.values))
 
 
 def prepare_df(unified_df: pd.DataFrame,
@@ -116,92 +107,93 @@ def generate_2_country_similarity_matrix(data_x: pd.DataFrame, data_y: pd.DataFr
 def menu_countries_similarity():
     number_of_docs = st.slider('Number of documents', min_value=1, max_value=50, step=1, value=10)
 
-    if st.button('Plot similarity'):
-        countries_similarity_matrix = generate_countries_similarity_matrix(unified_df=unified_df, pwdb_df=pwdb_df,
-                                                                           countries=countries,
-                                                                           number_of_docs=number_of_docs)
-        fig = px.imshow(countries_similarity_matrix,
-                        labels=dict(color="Semantic similarity"),
-                        x=countries,
-                        y=countries,
-                        width=700,
-                        height=700
-                        )
-        fig.update_xaxes(side="top")
-        st.plotly_chart(fig)
+    # if st.button('Plot similarity'):
+    #     countries_similarity_matrix = generate_countries_similarity_matrix(unified_df=unified_df, pwdb_df=pwdb_df,
+    #                                                                        countries=countries,
+    #                                                                        number_of_docs=number_of_docs)
+    #     fig = px.imshow(countries_similarity_matrix,
+    #                     labels=dict(color="Semantic similarity"),
+    #                     x=countries,
+    #                     y=countries,
+    #                     width=700,
+    #                     height=700
+    #                     )
+    #     fig.update_xaxes(side="top")
+    #     st.plotly_chart(fig)
 
 
 def menu_time_period_similarity():
     col1, col2, col3, col4 = st.columns(4)
-    country_1 = col1.selectbox('First country', countries)
-    country_2 = col2.selectbox('Second country', countries)
-    start_date = str(col3.date_input('start date', value=datetime(2020, 1, 1),
-                                     min_value=datetime(2020, 1, 1),
-                                     max_value=datetime(2021, 6, 1)))
-    end_date = str(col4.date_input('end date', value=datetime(2021, 6, 1),
-                                   min_value=datetime(2020, 1, 1),
-                                   max_value=datetime(2021, 6, 1)))
-    number_of_periods = st.slider('Periods', min_value=1, max_value=12, step=1, value=6)
-    number_of_docs = st.slider('Number of documents', min_value=1, max_value=50, step=1, value=10)
-    if st.button('Generate similarity'):
-        df_x = prepare_df(unified_df=unified_df,
-                          pwdb_df=pwdb_df,
-                          column_filter_name='country',
-                          column_filter_value=country_1
-                          )
-        df_y = prepare_df(unified_df=unified_df,
-                          pwdb_df=pwdb_df,
-                          column_filter_name='country',
-                          column_filter_value=country_2)
-        tmp_sim_matrix, tmp_periods = generate_2_country_similarity_matrix(df_x, df_y, start_date=start_date,
-                                                                           end_date=end_date, periods=number_of_periods,
-                                                                           number_of_docs = number_of_docs
-                                                                           )
-        tmp_periods = [' '.join([str(x), str(y)]) for x, y in tmp_periods]
-        color_scheme = [(0, "orange"),
-                        (0.5, "yellow"),
-                        (1, "lime")]
-        fig = px.imshow(tmp_sim_matrix,
-                        labels=dict(x=country_1, y=country_2, color="Semantic similarity"),
-                        x=tmp_periods,
-                        y=tmp_periods,
-                        width=800,
-                        height=700,
-                        zmin=0,
-                        zmax=1,
-                        color_continuous_scale=color_scheme
-                        )
-        fig.update_xaxes(side="top")
-        fig.update_xaxes(
-            showticklabels=True,
-            tickmode='linear',
-            tickfont=dict(
-                family='Old Standard TT, serif',
-                size=8,
-                color='black')
-        )
-        fig.update_yaxes(
-            scaleanchor="x",
-            scaleratio=1,
-            showticklabels=True,
-            tickmode='linear',
-            tickfont=dict(
-                family='Old Standard TT, serif',
-                size=8,
-                color='black'
-            )
-        )
-        st.plotly_chart(fig)
+    # country_1 = col1.selectbox('First country', countries)
+    # country_2 = col2.selectbox('Second country', countries)
+    # start_date = str(col3.date_input('start date', value=datetime(2020, 1, 1),
+    #                                  min_value=datetime(2020, 1, 1),
+    #                                  max_value=datetime(2021, 6, 1)))
+    # end_date = str(col4.date_input('end date', value=datetime(2021, 6, 1),
+    #                                min_value=datetime(2020, 1, 1),
+    #                                max_value=datetime(2021, 6, 1)))
+    # number_of_periods = st.slider('Periods', min_value=1, max_value=12, step=1, value=6)
+    # number_of_docs = st.slider('Number of documents', min_value=1, max_value=50, step=1, value=10)
+    # if st.button('Generate similarity'):
+    #     df_x = prepare_df(unified_df=unified_df,
+    #                       pwdb_df=pwdb_df,
+    #                       column_filter_name='country',
+    #                       column_filter_value=country_1
+    #                       )
+    #     df_y = prepare_df(unified_df=unified_df,
+    #                       pwdb_df=pwdb_df,
+    #                       column_filter_name='country',
+    #                       column_filter_value=country_2)
+    #     tmp_sim_matrix, tmp_periods = generate_2_country_similarity_matrix(df_x, df_y, start_date=start_date,
+    #                                                                        end_date=end_date, periods=number_of_periods,
+    #                                                                        number_of_docs = number_of_docs
+    #                                                                        )
+    #     tmp_periods = [' '.join([str(x), str(y)]) for x, y in tmp_periods]
+    #     color_scheme = [(0, "orange"),
+    #                     (0.5, "yellow"),
+    #                     (1, "lime")]
+    #     fig = px.imshow(tmp_sim_matrix,
+    #                     labels=dict(x=country_1, y=country_2, color="Semantic similarity"),
+    #                     x=tmp_periods,
+    #                     y=tmp_periods,
+    #                     width=800,
+    #                     height=700,
+    #                     zmin=0,
+    #                     zmax=1,
+    #                     color_continuous_scale=color_scheme
+    #                     )
+    #     fig.update_xaxes(side="top")
+    #     fig.update_xaxes(
+    #         showticklabels=True,
+    #         tickmode='linear',
+    #         tickfont=dict(
+    #             family='Old Standard TT, serif',
+    #             size=8,
+    #             color='black')
+    #     )
+    #     fig.update_yaxes(
+    #         scaleanchor="x",
+    #         scaleratio=1,
+    #         showticklabels=True,
+    #         tickmode='linear',
+    #         tickfont=dict(
+    #             family='Old Standard TT, serif',
+    #             size=8,
+    #             color='black'
+    #         )
+    #     )
+    #     st.plotly_chart(fig)
 
 
 def main():
-    st.title('Semantic similarity')
-    display_countries_similarity = st.checkbox('Countries similarity')
-    if display_countries_similarity:
-        menu_countries_similarity()
-    display_2_countries_similarity = st.checkbox('Time periods similarity between 2 countries')
-    if display_2_countries_similarity:
-        menu_time_period_similarity()
+    # st.title('Semantic similarity')
+    # display_countries_similarity = st.checkbox('Countries similarity')
+    # if display_countries_similarity:
+    #     menu_countries_similarity()
+    # display_2_countries_similarity = st.checkbox('Time periods similarity between 2 countries')
+    # if display_2_countries_similarity:
+    #     menu_time_period_similarity()
+    st.sidebar.title('')
 
 
 if __name__ == "__main__":
