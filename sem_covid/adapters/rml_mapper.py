2fc960137694f1bde141079944ba776f08e5d755
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2021-12-13 23:25:34 +0200

    Create rml_mapper.py

---
+++ sem_covid/adapters/rml_mapper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# rml_mapper.py
+# Date:  13.12.2021
+# Author: Stratulat Ștefan
+# Email: stefan.stratulat1997@gmail.com
+
+
+
+
+from abc import ABC, abstractmethod
+
+
+class Mapper(ABC):
+    pass
+
+
