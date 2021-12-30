cc94da2d3284398f1773914150c302ecdea93c76
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2021-12-30 20:17:12 +0200

    Update rml_mapper.py

--- sem_covid/adapters/rml_mapper.py
+++ sem_covid/adapters/rml_mapper.py
@@ -5,13 +5,31 @@
 # Author: Stratulat Ștefan
 # Email: stefan.stratulat1997@gmail.com
 
-
-
-
+import json
 from abc import ABC, abstractmethod
+import requests
 
 
-class Mapper(ABC):
-    pass
+class RMLMapperABC(ABC):
 
+    @abstractmethod
+    def transform(self, rml_rule: str, sources: dict) -> str:
+        pass
+
+
+class RMLMapper(RMLMapperABC):
+
+    def __init__(self,
+                 rml_mapper_url: str
+                 ):
+        self.rml_mapper_url = rml_mapper_url
+
+    def transform(self, rml_rule: str, sources: dict) -> str:
+        rml_mapper_query = {"rml": rml_rule, "sources": sources}
+        rml_mapper_result = requests.post(self.rml_mapper_url, json=rml_mapper_query)
+        if rml_mapper_result.ok:
+            return json.loads(rml_mapper_result.text)['output']
+        else:
+            print(rml_mapper_result)
+            return None
 
