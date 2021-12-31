1e55f46695c1379723aaa6d65e399b5784f35340
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2021-12-31 14:03:20 +0200

    Update rml_transform_dag.py

--- sem_covid/entrypoints/etl_dags/rml_transform_dag.py
+++ sem_covid/entrypoints/etl_dags/rml_transform_dag.py
@@ -54,18 +54,21 @@ def rml_transform_dag():
     @task
     def extract(rml_pipeline: RMLTransformPipeline):
         rml_pipeline.extract()
+        return rml_pipeline
 
     @task
     def transform(rml_pipeline: RMLTransformPipeline):
         rml_pipeline.transform()
-
+        return rml_pipeline
     @task
     def load(rml_pipeline: RMLTransformPipeline):
         rml_pipeline.load()
+        return rml_pipeline
 
-    extract(rml_transform_pipeline)
-    transform(rml_transform_pipeline)
-    load(rml_transform_pipeline)
+    pipeline_state = rml_transform_pipeline
+    pipeline_state = extract(pipeline_state)
+    pipeline_state = transform(pipeline_state)
+    pipeline_state = load(pipeline_state)
 
 
 etl_dag = rml_transform_dag()
